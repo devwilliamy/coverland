fbb81a1863ba980cbdb1b8a71d2fba2effee4c91
Femi Orokunle <156227418+femi-icarcover@users.noreply.github.com>
2024-01-19 13:04:19 -0800

    Installing ReactPlayer, removing default val
    - Installing ReactPlayer component for Vimeo link
    - Removing defaultValue attribute in select that was throwing error (default value is already set by useState)

--- src/components/PDP/ProductVideo.tsx
+++ src/components/PDP/ProductVideo.tsx
@@ -1,19 +1,22 @@
 'use client';
 
+import ReactPlayer from 'react-player/vimeo';
+
 export default function ProductVideo() {
   return (
     <div
       id="product-video"
       className="h-[370px] w-full max-w-full flex-col items-center justify-center overflow-hidden rounded-xl border lg:my-4 lg:flex"
     >
-      <iframe
+      <ReactPlayer
+        url="https://vimeo.com/904161479"
         width="100%"
         height="100%"
-        src="https://www.youtube.com/embed/TigF03eH0yE?controls=0&modestbranding=1&rel=0&autoplay=0"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
+        controls
+        muted
+        playing
+        loop
+      />
     </div>
   );
 }
