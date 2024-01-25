63841f61115b93e3724b9ec8381892478c348794
George Anumba <dev.george@icarcover.com>
2024-01-25 10:42:02 -0800

    - Added Plus Icon back to AccordionTrigger

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import * as AccordionPrimitive from '@radix-ui/react-accordion';
 
 import { cn } from '@/lib/utils';
+import { FaPlus } from 'react-icons/fa6';
 
 const Accordion = AccordionPrimitive.Root;
 
@@ -34,7 +35,7 @@ const AccordionTrigger = React.forwardRef<
     >
       {children}
       {/* <ChevronDown className="h-6 w-6 shrink-0 transition-transform duration-200" /> */}
-      {/* <FaPlus className="h-6 w-6 shrink-0 transition-transform duration-200" /> */}
+      <FaPlus className="h-6 w-6 shrink-0 transition-transform duration-200" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ));
