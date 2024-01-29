d4e284ef787e6a9f833da1c4469684a6a8e039a3
William Yang <dev.william.y@icarcover.com>
2024-01-29 11:12:58 -0800

    Uploaded some videos

--- src/components/PDP/ProductVideo.tsx
+++ src/components/PDP/ProductVideo.tsx
@@ -3,7 +3,10 @@
 import { VIMEO_VIDEO_LINK } from '@/lib/constants';
 // import ReactPlayer from 'react-player/vimeo';
 import Video from 'next-video';
-// import awesomeVideo from 'https://stream.mux.com/Xidr94DjL01AsdkdKymFNOdzpDDcVKjQTUR9008RcOEgs.m3u8';
+// import awesomeVideo from '../../../videos/file_example_MP4_1920_18MG.mp4';
+import awesomeVideo from '../../../videos/Premium Plus Car Cover_1080p.mp4';
+import MuxPlayer from '@mux/mux-player-react';
+
 export default function ProductVideo() {
   return (
     <div
@@ -18,11 +21,15 @@ export default function ProductVideo() {
         muted
         loop
       /> */}
-      <Video
-        src={
-          'https://stream.mux.com/Xidr94DjL01AsdkdKymFNOdzpDDcVKjQTUR9008RcOEgs.m3u8'
-        }
-      />
+      <Video src={awesomeVideo} />
+      {/* <MuxPlayer
+        streamType="on-demand"
+        playbackId="1m1U9h6TdDlVEkMYqrHYmZ7O5exAyrAtvB5vq01u9wug"
+        metadataVideoTitle="Placeholder (optional)"
+        metadataViewerUserId="Placeholder (optional)"
+        primaryColor="#FFFFFF"
+        secondaryColor="#000000"
+      /> */}
     </div>
   );
 }
