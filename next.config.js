39b6187690418b1c6b5abea4364e5c2eb425e330
William Yang <dev.william.y@icarcover.com>
2024-01-29 10:22:35 -0800

    First sample on mux on carousel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-import { withNextVideo } from 'next-video/process';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withNextVideo } = require('next-video/process');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
--- src/components/PDP/ProductVideo.tsx
+++ src/components/PDP/ProductVideo.tsx
@@ -1,21 +1,27 @@
 'use client';
 
 import { VIMEO_VIDEO_LINK } from '@/lib/constants';
-import ReactPlayer from 'react-player/vimeo';
-
+// import ReactPlayer from 'react-player/vimeo';
+import Video from 'next-video';
+// import awesomeVideo from 'https://stream.mux.com/Xidr94DjL01AsdkdKymFNOdzpDDcVKjQTUR9008RcOEgs.m3u8';
 export default function ProductVideo() {
   return (
     <div
       id="product-video"
-      className="flex flex-col items-center h-[250px] w-full lg:my-4 lg:flex lg:h-[500px]"
+      className="flex h-[250px] w-full flex-col items-center lg:my-4 lg:flex lg:h-[500px]"
     >
-      <ReactPlayer
+      {/* <ReactPlayer
         url={VIMEO_VIDEO_LINK}
         width="100%"
         height="100%"
         controls
         muted
         loop
+      /> */}
+      <Video
+        src={
+          'https://stream.mux.com/Xidr94DjL01AsdkdKymFNOdzpDDcVKjQTUR9008RcOEgs.m3u8'
+        }
       />
     </div>
   );
