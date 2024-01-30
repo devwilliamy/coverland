dad4115b7a56a4ce4471a65445f99a6b360558c9
William Yang <dev.william.y@icarcover.com>
2024-01-29 17:55:55 -0800

    Moving to OG mux player

--- src/components/PDP/ProductVideo.tsx
+++ src/components/PDP/ProductVideo.tsx
@@ -1,5 +1,6 @@
 'use client';
-import Video from 'next-video';
+// import Video from 'next-video';
+import MuxPlayer from '@mux/mux-player-react';
 
 export default function ProductVideo() {
   return (
@@ -7,11 +8,16 @@ export default function ProductVideo() {
       id="product-video"
       className="flex flex-col lg:my-4 lg:flex lg:h-[500px]"
     >
-      <Video
+      <MuxPlayer
         // playbackId="4gDB6CEIClCYETrwhCi6IfDQaHpID9r4HmS6rR18Hhc" // Square Ratio Sample.mp4
         playbackId="x02004Q3mOljwEZKIj5cyWebBI9ioTGBsNUG01pN4JZo4k" // Square Ratio.mp4
         muted
       />
+      {/* <Video
+        // playbackId="4gDB6CEIClCYETrwhCi6IfDQaHpID9r4HmS6rR18Hhc" // Square Ratio Sample.mp4
+        playbackId="x02004Q3mOljwEZKIj5cyWebBI9ioTGBsNUG01pN4JZo4k" // Square Ratio.mp4
+        muted
+      /> */}
     </div>
   );
 }
