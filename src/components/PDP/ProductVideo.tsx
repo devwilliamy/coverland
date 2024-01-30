d895a8cbe90093cff10e585d50bc5a61abe27f40
William Yang <dev.william.y@icarcover.com>
2024-01-30 11:59:55 -0800

    Square video should work now

--- src/components/PDP/ProductVideo.tsx
+++ src/components/PDP/ProductVideo.tsx
@@ -1,23 +1,6 @@
 'use client';
-// import Video from 'next-video';
-import MuxPlayer from '@mux/mux-player-react';
-
+import Video from 'next-video';
+import SquareVideo from '../../../videos/Square Ratio.mp4';
 export default function ProductVideo() {
-  return (
-    <div
-      id="product-video"
-      className="flex flex-col lg:my-4 lg:flex lg:h-[500px]"
-    >
-      <MuxPlayer
-        // playbackId="4gDB6CEIClCYETrwhCi6IfDQaHpID9r4HmS6rR18Hhc" // Square Ratio Sample.mp4
-        playbackId="x02004Q3mOljwEZKIj5cyWebBI9ioTGBsNUG01pN4JZo4k" // Square Ratio.mp4
-        muted
-      />
-      {/* <Video
-        // playbackId="4gDB6CEIClCYETrwhCi6IfDQaHpID9r4HmS6rR18Hhc" // Square Ratio Sample.mp4
-        playbackId="x02004Q3mOljwEZKIj5cyWebBI9ioTGBsNUG01pN4JZo4k" // Square Ratio.mp4
-        muted
-      /> */}
-    </div>
-  );
+  return <Video src={SquareVideo} muted style={{ aspectRatio: 4 / 3 }} />;
 }
