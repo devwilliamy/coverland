d97205c05654bd9025b27933db5db77e3ae00b53
George Anumba <dev.george@icarcover.com>
2024-01-30 14:07:43 -0800

    Fixed Suv Cover Grid at medium and large Breakpoints

--- src/components/home/CoversGrid.tsx
+++ src/components/home/CoversGrid.tsx
@@ -12,14 +12,14 @@ const coverTypes = [
 
 const CoversGrid = () => {
   return (
-    <span className="mt-[-70px] h-full flex-col items-center lg:flex lg:px-[80px]">
+    <span className="mt-[-70px] h-full flex-col items-center px-4 lg:flex lg:px-[80px]">
       <div className="grid h-full max-w-[1280px]  grid-cols-2 grid-rows-2 place-items-center gap-[7px] lg:gap-[20px]  ">
         {coverTypes.map(({ title, img }) => (
           <div
             key={`${title}-block`}
-            className="flex min-w-[167px] max-w-[167px] flex-col items-center justify-center rounded-[8px] p-[10px] shadow-md lg:flex-row lg:gap-[36px] lg:px-[50px]"
+            className="flex min-w-[167px]  flex-col items-center justify-center rounded-[8px] p-[10px] shadow-md lg:flex-row lg:gap-[36px] lg:px-[50px]"
           >
-            <p className="order-last flex min-w-[45%] flex-[0.1] justify-center whitespace-nowrap text-center align-middle text-[14px] font-[900] uppercase lg:w-1/2 lg:max-w-[45%] lg:text-[24px]">
+            <p className="order-last flex min-w-[45%] flex-[0.1] justify-center whitespace-nowrap text-center align-middle text-[14px] font-[900] uppercase lg:order-first lg:w-1/2 lg:max-w-[45%] lg:text-[24px]">
               {title}
             </p>
             <div className="flex flex-[0.9] lg:h-full lg:w-full ">
