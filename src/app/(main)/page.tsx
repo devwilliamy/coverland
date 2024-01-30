44d594dbe8577e330c4a2d09c5f4bdccab6f2651
Femi Orokunle <156227418+femi-icarcover@users.noreply.github.com>
2024-01-30 15:07:51 -0800

    Revert "empty commit for deployment"
    This reverts commit e339387365f0a3a8c0c0e70e475deb8a0d18b37a.

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
       <Suspense>
         <HeroSection />
       </Suspense>
-
       {/* <CategoryCards /> */}
       <TrustBanner />
       {/* <SolutionSection /> */}
