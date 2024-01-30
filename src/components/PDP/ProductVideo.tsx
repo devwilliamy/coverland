dba52b82cadca99a17d4d4838807470b7bbb56c5
William Yang <dev.william.y@icarcover.com>
2024-01-29 17:38:51 -0800

    Square ratio video is up

--- src/components/PDP/ProductVideo.tsx
+++ src/components/PDP/ProductVideo.tsx
@@ -8,6 +8,7 @@ export default function ProductVideo() {
       className="flex flex-col lg:my-4 lg:flex lg:h-[500px]"
     >
       <Video
+        // playbackId="4gDB6CEIClCYETrwhCi6IfDQaHpID9r4HmS6rR18Hhc" // Square Ratio Sample.mp4
         playbackId="x02004Q3mOljwEZKIj5cyWebBI9ioTGBsNUG01pN4JZo4k" // Square Ratio.mp4
         muted
       />
