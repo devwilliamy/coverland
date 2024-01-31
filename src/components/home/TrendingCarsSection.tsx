04d719227e6806792c054c89404451555ba628f8
George Anumba <dev.george@icarcover.com>
2024-01-30 17:33:48 -0800

    - Merged main and removed unused imports - Added arrows to Trending car brands

--- src/components/home/TrendingCarsSection.tsx
+++ src/components/home/TrendingCarsSection.tsx
@@ -4,6 +4,7 @@ import Ram from '@/images/hero/trending-ram.png';
 import Dodge from '@/images/hero/trending-dodge.png';
 import Chevy from '@/images/hero/trending-chevy.png';
 import Image from 'next/image';
+import { ArrowRight } from 'lucide-react';
 // import "./"
 
 const TrendingCars = [
@@ -36,22 +37,23 @@ const TrendingCarsSection = () => {
                 </p>
                 <p className=" mb-[18px] text-[14px] font-[500] leading-[24px] tracking-wide text-white">
                   See All-{title} <br />
-                  <button
-                    className=""
-                    //  className="flex h-[44px] items-center rounded-[100px] px-[40px] py-[17px] text-[16px] font-[900] leading-[110%] tracking-[0.32px] outline outline-[1px]"
-                  >
-                    Custom Car Covers
-                  </button>
+                  <div className="flex items-end gap-2">
+                    <div>Custom Car Covers</div>
+                    <ArrowRight className="max-h-[24px] max-w-[16px]" />
+                  </div>
                 </p>
               </div>
 
-              <div className="absolute bottom-[55px] left-[35px] hidden lg:block">
+              <div className="absolute bottom-[35px] left-[25px] hidden lg:block">
                 <p className="mb-[20px] text-[38px] font-black leading-[24px] tracking-wide text-white">
                   {title}
                 </p>
                 <p className="mb-[18px] text-[16px] font-[500] leading-[24px] tracking-wide text-white">
                   See All-{title} <br />
-                  <button>Custom Car Covers</button>
+                  <div className="flex items-end gap-2">
+                    <div>Custom Car Covers</div>
+                    <ArrowRight className="max-h-[24px] max-w-[16px]" />
+                  </div>
                 </p>
               </div>
             </a>
