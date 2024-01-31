f78ba77ddff2eba0295d9b2b247d26cb3e674635
William Yang <dev.william.y@icarcover.com>
2024-01-31 12:28:13 -0800

    Best selling section lost its center idk what happened

--- src/components/home/BestSellingSection.tsx
+++ src/components/home/BestSellingSection.tsx
@@ -10,7 +10,7 @@ const bestSelling = [
   { title: 'Dodge Challenger', img: Challenger },
   { title: 'Chevy Corvette', img: Corvette },
   { title: 'Mazda Miata', img: Miata },
-  { title: 'Chevy El-camino', img: ElCamino },
+  { title: 'Chevy El-Camino', img: ElCamino },
 ];
 const BestSellingSection = () => {
   return (
@@ -18,7 +18,7 @@ const BestSellingSection = () => {
       <p className="text-[20px] font-black lg:text-[32px]">
         BEST-SELLING CAR MODELS
       </p>
-      <div className="max-w-screen flex gap-[20px] overflow-x-auto">
+      <div className="max-w-screen flex gap-[20px] overflow-x-auto pb-3">
         {bestSelling.map(({ title, img }, i) => (
           <a
             key={`${title}-${i}`}
@@ -26,14 +26,12 @@ const BestSellingSection = () => {
             className="flex min-h-[209px] min-w-[209px] flex-[25%] flex-col items-center justify-between"
           >
             <Image alt="Best-Selling-Car-Cover" className="" src={img} />
-            <div className="flex flex-col">
-              <p className="mb-[18px] whitespace-nowrap text-[16px] font-[500] leading-[24px] lg:text-[22px]">
-                {title}
-              </p>
-              <button className="mb-[2px] flex h-[44px] max-w-[153px] items-center justify-center whitespace-nowrap rounded-[100px] px-[40px] py-[17px] text-[16px] font-[900] leading-[110%] tracking-[0.32px] outline outline-[1px]">
-                <div>Shop Now</div>
-              </button>
-            </div>
+            <p className="mb-[18px] whitespace-nowrap text-[16px] font-[500] leading-[24px] lg:text-[22px]">
+              {title}
+            </p>
+            <button className="mb-[2px] flex h-[44px] max-w-[153px] items-center justify-center whitespace-nowrap rounded-[100px] px-[40px] py-[17px] text-[16px] font-[900] leading-[110%] tracking-[0.32px] outline outline-[1px]">
+              <div>Shop Now</div>
+            </button>
           </a>
         ))}
       </div>
