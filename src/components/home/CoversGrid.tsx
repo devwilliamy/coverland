6345da3c599908d1dd422299776c8e191bd085d1
William Yang <dev.william.y@icarcover.com>
2024-01-31 10:01:49 -0800

    Disabled SUV and Truck Cover

--- src/components/home/CoversGrid.tsx
+++ src/components/home/CoversGrid.tsx
@@ -4,7 +4,6 @@ import CAR from '@/images/hero/car-cover-home-icon.png';
 import TRUCK from '@/images/hero/truck-cover-home-icon.png';
 import SUV from '@/images/hero/suv-cover-home-icon.png';
 
-
 const coverTypes = [
   { title: 'Car Covers', img: CAR },
   { title: 'Truck Covers', img: TRUCK },
@@ -16,9 +15,12 @@ const CoversGrid = () => {
     <span className="mt-[-70px] h-full flex-col items-center px-4 lg:flex lg:px-[80px]">
       <div className="grid h-full max-w-[1280px]  grid-cols-2 grid-rows-2 place-items-center gap-[7px] lg:gap-[20px]  ">
         {coverTypes.map(({ title, img }) => (
-          <div key={`${title}-block`}>
+          <div
+            key={`${title}-block`}
+            className={`${title !== 'Car Covers' ? 'pointer-events-none' : ''}`}
+          >
             <a
-              href="/car-covers"
+              href={'/car-covers'}
               className="flex min-w-[167px]  flex-col items-center justify-center rounded-[8px] p-[10px] shadow-md lg:flex-row lg:gap-[36px] lg:px-[50px]"
             >
               <p className="order-last flex min-w-[45%] flex-[0.1] justify-center whitespace-nowrap text-center align-middle text-[14px] font-[900] uppercase lg:order-first lg:w-1/2 lg:max-w-[45%] lg:text-[24px]">
