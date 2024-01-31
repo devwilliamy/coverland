10e1d2316dd19c26cdf2ed99bd2f412241eead59
William Yang <dev.william.y@icarcover.com>
2024-01-31 11:46:53 -0800

    Removed padding

--- src/pages/home/HomepageVideoSection.tsx
+++ src/pages/home/HomepageVideoSection.tsx
@@ -14,7 +14,7 @@ export default function HomepageVideoSection() {
       <p className="mb-[40px] hidden text-center text-[45px] font-black uppercase leading-[36px] lg:flex ">
         Perfect Solution for All Weather
       </p>
-      <span className="flex w-full flex-col items-center bg-[#1A1A1A] px-[18px] pt-[40px] lg:px-[150px] lg:pt-[92px]">
+      <span className="flex w-full flex-col items-center bg-[#1A1A1A] pt-[40px] lg:px-[150px] lg:pt-[92px]">
         <ProductVideo />
         <button className="my-[30px] flex min-h-[48px] w-full max-w-[214px]  flex-col items-center justify-center rounded-[4px] bg-[#BE1B1B] px-[40px] py-[15px] text-[16px] font-black text-white ">
           <a href="/car-covers">Shop Now</a>
