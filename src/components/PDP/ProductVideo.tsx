d738910261b8ee97af0f6bdb8fa092eda85647c7
William Yang <dev.william.y@icarcover.com>
2024-01-30 17:21:50 -0800

    Update height stylin to make it fit

--- src/components/PDP/ProductVideo.tsx
+++ src/components/PDP/ProductVideo.tsx
@@ -2,5 +2,11 @@
 import Video from 'next-video';
 import SquareVideo from '../../../videos/Square Ratio.mp4';
 export default function ProductVideo() {
-  return <Video src={SquareVideo} muted style={{ aspectRatio: 4 / 3 }} />;
+  return (
+    <Video
+      src={SquareVideo}
+      muted
+      style={{ aspectRatio: 4 / 3, height: '100%' }}
+    />
+  );
 }
