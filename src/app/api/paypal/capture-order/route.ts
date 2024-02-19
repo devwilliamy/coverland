7df6d349f1da9b2b5d9ddb7369bee4d6f682395f
Femi Orokunle <femiorokunle@gmail.com>
2024-02-19 08:02:36 -0800

    Minor updates

--- src/app/api/paypal/capture-order/route.ts
+++ src/app/api/paypal/capture-order/route.ts
@@ -3,6 +3,9 @@ import paypal from '@paypal/checkout-server-sdk';
 import { PaypalClient } from '../utils';
 
 export async function POST(req: Request) {
+  if (req.method != 'POST') {
+    return Response.json({ success: false, message: 'Not Found' });
+  }
   const body = await req.json();
 
   if (!body.orderID) {
@@ -22,24 +25,6 @@ export async function POST(req: Request) {
   } catch (error) {
     console.log(error);
   }
-
-  // const request = new paypal.orders.OrdersCaptureRequest(orderID);
-  // console.log(request);
-
-  // request.requestBody({ payment_source: { token: body.token } });
-
-  // console.log(response);
-  // console.log('here');
-  // console.log(response);
-
-  // if (!response) {
-  //   console.log('here');
-
-  //   return Response.json({
-  //     success: false,
-  //     message: 'Some Error Occured at backend',
-  //   });
-  // }
 
   return Response.json({ success: true, data: '' });
 }
