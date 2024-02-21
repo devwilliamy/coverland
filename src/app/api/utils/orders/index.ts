d9b608a15a02c3bea38d5b0d3f6687398682751b
Femi Orokunle <femiorokunle@gmail.com>
2024-02-20 16:17:56 -0800

    Fix for total value

--- src/app/api/utils/orders/index.ts
+++ src/app/api/utils/orders/index.ts
@@ -102,7 +102,7 @@ export const handleAddOrderId = async ({
       error = await supabase.from('_Orders').insert({
         order_id: order_id,
         skus: cartItems.map((item) => item.sku),
-        total,
+        total: Number(total.toFixed(2)),
       });
     }
 
