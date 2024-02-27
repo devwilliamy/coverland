b0029fb46fe1ed28de278e23b47b85eda8750dc1
Femi Orokunle <156227418+femi-icarcover@users.noreply.github.com>
2024-02-26 19:59:36 -0800

    .

--- src/app/api/json-data/route.ts
+++ src/app/api/json-data/route.ts
@@ -1,16 +1,18 @@
 import { readFile } from 'fs/promises';
+import path from 'path';
 
 export async function GET(request: Request) {
   const url = new URL(request.url);
   const type = url.searchParams.get('type');
   const make = url.searchParams.get('make');
-
+  const jsonPath = path.join(
+    process.cwd(),
+    'public',
+    `${type}/${make}_${type?.replace(/-/g, '_').toLowerCase()}.json`
+  );
+  console.log('jsonpath', jsonPath);
   try {
-    const data = await readFile(
-      process.cwd() +
-        `/${type}/${make}_${type?.replace(/-/g, '_').toLowerCase()}.json`,
-      'utf-8'
-    );
+    const data = await readFile(jsonPath, 'utf-8');
     console.log(data);
     const jsonData = JSON.parse(data);
     console.log(jsonData);
