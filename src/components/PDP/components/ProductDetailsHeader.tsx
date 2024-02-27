883030a0cb1caa68c0d43cd4bea68e333fa0ae19
William Yang <dev.william.y@icarcover.com>
2024-02-26 18:15:55 -0800

    Fixed an early return for product details header

--- src/components/PDP/components/ProductDetailsHeader.tsx
+++ src/components/PDP/components/ProductDetailsHeader.tsx
@@ -13,11 +13,9 @@ export default function ProductDetailsHeader() {
   const make = params?.make;
   let productType = params?.productType;
   if (productType === 'truck-covers') {
-    return (productType = 'Truck');
-  } else {
-    if (productType === 'suv-covers') {
-      productType = productType.toUpperCase();
-    }
+    productType = 'Truck';
+  } else if (productType === 'suv-covers') {
+    productType = productType.toUpperCase();
     productType = productType?.slice(0, 3);
   }
 
