04b5767232fe97c7b71f669b68ccd1397059a69b
George Anumba <dev.george@icarcover.com>
2024-03-04 12:39:59 -0800

    - updated banner images for desktop and mobile

--- src/components/hero/HeroSection.tsx
+++ src/components/hero/HeroSection.tsx
@@ -2,22 +2,24 @@
 
 import { useMediaQuery } from '@mantine/hooks';
 import { HeroDropdown } from './dropdown/HeroDropdown';
-import hero from '@/images/hero/hero.webp';
+// import hero from '@/images/hero/hero.webp';
+import hero from '@/images/hero/home-hero_D.webp';
 import heroMobile from '@/images/hero/hero_mobile.webp';
 import Image from 'next/image';
 
 const HeroSection = () => {
   const isMobile = useMediaQuery('(max-width: 500px)');
   return (
-    <section className="flex h-full flex-col items-center justify-end px-4 lg:justify-start lg:px-0">
-      <div className="relative flex h-full min-h-[550px] w-full flex-col justify-end ">
+    <section className=" flex h-full flex-col items-center px-4 lg:justify-center lg:px-0">
+      <div className="relative flex h-full  w-full flex-col justify-end ">
         <Image
-          className="absolute"
+          height={800}
+          width={800}
+          className="-mt-0.5 w-full"
           src={isMobile ? heroMobile.src : hero.src}
           alt="hero"
-          fill
         />
-        <div className="relative flex h-full flex-col   pb-2 text-center">
+        <div className="absolute w-full flex-col justify-center   pb-2 text-center">
           <p className="text-xs uppercase tracking-wider text-white lg:my-2 lg:text-2xl">
             #1 Rated Car Cover in the USA
           </p>
