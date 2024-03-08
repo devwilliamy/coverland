c8c7aa4bb65831b0ff2240d202717a77f65d9741
George Anumba <dev.george@icarcover.com>
2024-03-07 16:51:29 -0800

    - Added add to cart drawer for seat covers

--- src/app/(main)/seat-covers/components/SeatContent.tsx
+++ src/app/(main)/seat-covers/components/SeatContent.tsx
@@ -1,10 +1,22 @@
+'use client';
 import Image, { StaticImageData } from 'next/image';
-import React, { SetStateAction } from 'react';
+import React, { SetStateAction, useEffect, useState } from 'react';
 import SeatCoverFreeDetails from './SeatCoverFreeDetails';
 import CompatibleVehiclesTrigger from './CompatibleVehiclesTrigger';
 import installments from '@/images/PDP/Product-Details-Redesign-2/paypal-installments.webp';
-import { Rating } from '@mui/material';
+import { Rating, useMediaQuery } from '@mui/material';
 import { SeatData, SeatImageDataObject, SeatString } from '../util';
+import { Button } from '@/components/ui/button';
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { Check, X } from 'lucide-react';
+import FrontCovers from '@/images/PDP/Product-Details-Redesign-2/seat-covers/front-covers.webp';
+import BackCovers from '@/images/PDP/Product-Details-Redesign-2/seat-covers/back-covers.webp';
 
 const seatColors: { color: SeatString; data: SeatData }[] = [
   { color: 'BlackRedData', data: SeatImageDataObject.BlackRedData },
@@ -22,6 +34,76 @@ export default function SeatContent({
   colorIndex: number;
   setColorIndex: React.Dispatch<SetStateAction<number>>;
 }) {
+  const isMobile = useMediaQuery('(max-width:1024px)');
+  const coverPrice = 99.95;
+  const [selectedCovers, setSelectedCovers] = useState<string[]>([]);
+  const [total, setTotal] = useState(0);
+  const seatSelectedStyle =
+    'bg-white text-black hover:bg-black hover:text-white';
+  const seatDeselectedStyle = 'bg-black hover:bg-white hover:text-black';
+
+  const SeatOption = ({
+    src,
+    option,
+  }: {
+    src: StaticImageData;
+    option: 'front' | 'back';
+  }) => {
+    return (
+      <span className="flex min-h-[150px] w-full items-center justify-between gap-3.5 overflow-hidden rounded-md bg-white ">
+        <Image
+          alt={option + '-covers'}
+          src={src}
+          className="max-h-[88px] w-1/2 max-w-[139px] pl-4"
+        />
+        <div className="flex w-1/2 flex-col pr-4">
+          <p className="whitespace-nowrap text-[16px] font-[700] capitalize leading-[29px]">
+            {option} seat covers
+          </p>
+          <div className="flex items-center gap-1 text-[14px] leading-[26px]">
+            <p className="font-[700]">${coverPrice}</p>
+            <p className="text-[#9C9C9C]">$200</p>
+            <p className="text-[#BE1B1B]">(-50%)</p>
+          </div>
+          <Button
+            onClick={() => {
+              if (selectedCovers.includes(option)) {
+                setTotal((e) => {
+                  if (e > coverPrice) {
+                    return e - coverPrice;
+                  }
+                  return 0;
+                });
+              } else {
+                setTotal((e) => {
+                  return e + coverPrice;
+                });
+              }
+              setSelectedCovers((e) => {
+                if (e.includes(option)) {
+                  return e.filter((e) => e !== option);
+                }
+
+                return [...e, option];
+              });
+            }}
+            className={`mt-4  flex gap-1 ${selectedCovers.includes(option) ? seatSelectedStyle : seatDeselectedStyle} uppercase  outline outline-[1px] `}
+          >
+            {selectedCovers.includes(option) ? (
+              <>
+                <Check className="text-[#43A047]" />
+                <p>Selected</p>
+              </>
+            ) : (
+              <p>Select</p>
+            )}
+          </Button>
+        </div>
+      </span>
+    );
+  };
+  useEffect(() => {}, [total, selectedCovers]);
+
   return (
     <section className="flex h-full w-full flex-col max-lg:px-4 max-lg:pt-[34px] lg:sticky lg:top-8 lg:w-1/2">
       <div className="flex flex-col ">
@@ -90,6 +172,43 @@ export default function SeatContent({
       </section>
       <SeatCoverFreeDetails />
       <CompatibleVehiclesTrigger />
+      <Sheet>
+        <SheetTrigger className="mb-[37px]   lg:mb-0">
+          <Button className=" h-full min-h-[48px] w-full bg-[#BE1B1B] uppercase ">
+            Add to Cart
+          </Button>
+        </SheetTrigger>
+        <SheetContent
+          side={isMobile ? 'bottom' : 'right'}
+          className={`${isMobile ? 'min-h-[75vh] rounded-t-lg ' : 'w-[30vw]'} flex flex-col justify-between  bg-[#323232]`}
+        >
+          <SheetHeader className="flex w-full flex-col items-end">
+            <SheetClose className="mr-4 mt-[20px] rounded-full bg-[#F0F0F099] p-1.5">
+              <X size={28} />
+            </SheetClose>
+          </SheetHeader>
+          <section className={`flex w-full flex-col px-5`}>
+            <div className="flex flex-col gap-4 ">
+              <p className="w-full pb-2 pt-[30px] text-center text-[24px] font-black uppercase leading-[29px] text-white lg:mt-auto lg:pb-[50px]">
+                select your option
+              </p>
+              <SeatOption option="front" src={FrontCovers} />
+              <SeatOption option="back" src={BackCovers} />
+            </div>
+          </section>
+          <span className="flex min-h-[126px] w-full flex-col items-center justify-center self-end bg-white px-4">
+            <p className="w-full pb-5 text-end text-[16px] font-black leading-[19px] ">
+              Total: ${Number(total).toFixed(2)}
+            </p>
+            <Button
+              disabled={total <= 0}
+              className={`max-h-[48px] min-h-[48px] w-full ${total <= 0 ? 'disabled:bg-[#BE1B1B80]' : 'bg-[#BE1B1B]'} uppercase lg:max-h-[62px] lg:min-h-[62px] `}
+            >
+              Add to Cart
+            </Button>
+          </span>
+        </SheetContent>
+      </Sheet>
     </section>
   );
 }
