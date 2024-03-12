e94a20384ef0f400fb693374bcfe09735c31f96c
George Anumba <dev.george@icarcover.com>
2024-03-12 16:39:37 -0700

    - Minor styling changes

--- src/app/(main)/not-found.tsx
+++ src/app/(main)/not-found.tsx
@@ -2,10 +2,16 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div className="w-full">
+    <div className="flex w-full flex-col items-center">
+      <h5 className="text-[100px]">404</h5>
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+      <Link
+        className="my-4 rounded-[8px] bg-[#B9130C] px-20 py-4 text-[20px] font-[700]  text-white "
+        href="/"
+      >
+        Return Home
+      </Link>
     </div>
   );
 }
