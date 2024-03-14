0cfc78dbdd36b79cad02a43a546b703dc2b3355e
George Anumba <dev.george@icarcover.com>
2024-03-14 16:36:44 -0700

    - Fixing contact page funcitonality

--- src/app/(main)/contact/page.tsx
+++ src/app/(main)/contact/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Banner from '@/images/hero/hero.webp';
 import Image from 'next/image';
 import { Raleway } from 'next/font/google';
@@ -7,6 +8,8 @@ import Chat from '@/images/contact/chat 2.webp';
 import Pin from '@/images/contact/location 1.webp';
 import Mail from '@/images/contact/mail 1.webp';
 import { Separator } from '@/components/ui/separator';
+import { ChangeEvent, FormEvent, FormEventHandler, useState } from 'react';
+import { sendEmail } from './util';
 
 const raleway = Raleway({
   weight: ['100', '400', '700', '900'],
@@ -29,12 +32,122 @@ const contactGrid = [
   { img: Phone, text: '(800)-799-5165' },
   { img: Mail, text: 'info@coverland.com' },
 ];
+type validObj = {
+  value: string;
+  errors: boolean;
+  errorMessage: string;
+  firstVisit: boolean;
+};
 
 const Contact = () => {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  const phoneRegex = /^\+(?:[0-9] ?){6,14}[0-9]$/;
+  const [validationObject, setValidationObject] = useState<
+    Record<string, validObj>
+  >({
+    email: {
+      value: '',
+      errors: false,
+      errorMessage: '',
+      firstVisit: true,
+    },
+    phone: {
+      value: '',
+      errors: false,
+      errorMessage: '',
+      firstVisit: true,
+    },
+  });
+  const validateEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const isValid = emailRegex.test(value);
+    if (!isValid) {
+      console.log(value);
+      setValidationObject((e) => {
+        return {
+          ...e,
+          email: {
+            ...e.email,
+            errors: true,
+            errorMessage: 'Invalid Email',
+          },
+        };
+      });
+      return;
+    }
+    setValidationObject((e) => {
+      return {
+        ...e,
+        email: {
+          ...e.email,
+          errors: false,
+          errorMessage: '',
+        },
+      };
+    });
+  };
+  const validatePhone = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const isValid = phoneRegex.test(value);
+    if (!isValid) {
+      console.log(value);
+      setValidationObject((e) => {
+        return {
+          ...e,
+          phone: {
+            ...e.phone,
+            errors: true,
+            errorMessage: 'Invalid Phone Number',
+          },
+        };
+      });
+      return;
+    }
+    setValidationObject((e) => {
+      return {
+        ...e,
+        phone: {
+          ...e.phone,
+          errors: false,
+          errorMessage: '',
+        },
+      };
+    });
+  };
+  const handleSubmit = (formData: FormData) => {
+    let subject: string | unknown;
+    let body: string | unknown;
+    let email: string | unknown;
+    let phoneNumber: string | unknown;
+    if (formData) {
+      email = formData.get('email')?.valueOf();
+      phoneNumber = formData.get('phoneNumber')?.valueOf();
+      subject = formData.get('subject')?.valueOf();
+      body = formData.get('body')?.valueOf();
+      window.location.href = `mailto:?subject=${subject}&body=${email}%0A${phoneNumber}%0A${body}`;
+      return;
+    }
+  };
+
+  const handlePhoneChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (isNaN(parseFloat(e.target.value))) {
+      return;
+    }
+    setValidationObject((state) => {
+      return {
+        ...state,
+        phone: {
+          ...state.phone,
+          value: e.target.value,
+        },
+      };
+    });
+    validatePhone(e);
+  };
+
   return (
     <section className="flex w-full flex-col">
       <header className="relative h-28 overflow-hidden lg:h-44">
-        {/* <div className="absolute inset-0 bg-gradient-to-r from-[rgba(0,0,0,0.50)] from-0% via-[rgba(0,0,0,0.50)] via-100%"></div> */}
         <Image
           className="w-full bg-gray-300 bg-no-repeat object-contain"
           alt="coverland-banner"
@@ -64,52 +177,102 @@ const Contact = () => {
         ))}
       </span>
       <Separator />
-      <span className={`px-5 pt-[48px]`}>
+      <form
+        id="form"
+        method="post"
+        encType="text/plain"
+        className={`px-5 pt-[48px]`}
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.currentTarget);
+          handleSubmit(formData);
+        }}
+      >
         <p className="pb-[48px] text-[16px] leading-[21px] lg:text-[16px] lg:leading-[21px]">
           We love to hear from you! Please let us know if you have any questions
           or concerns and we will get back to you shortly. Thanks!
         </p>
-        <label htmlFor="Name" className="pb-2 font-black">
+        <p className="pb-2">* Required</p>
+        <label htmlFor="subject" className="pb-2 font-black">
           Name *
         </label>
         <input
-          id="Email"
+          id="subject"
+          name="subject"
+          placeholder="John Doe"
           type="text"
           required
           className="mb-[13px] min-h-[50px] w-full border-[2px] border-[#DBDBDB]"
         />
-        <label htmlFor="Email" className="pb-2 font-black">
+        <label htmlFor="email" className="pb-2 font-black">
           Email *
         </label>
         <input
-          id="Phone Number"
+          id="email"
+          name="email"
+          placeholder="JohnDoe@email.com"
           type="text"
+          required
+          onChange={(e) => {
+            console.log(validationObject.email.firstVisit);
+            !validationObject.email.firstVisit && validateEmail(e);
+          }}
+          onBlur={(e) => {
+            setValidationObject((e) => {
+              return {
+                ...e,
+                email: {
+                  ...e.email,
+                  firstVisit: false,
+                },
+              };
+            });
+            validateEmail(e);
+          }}
           className="mb-[13px] min-h-[50px] w-full border-[2px] border-[#DBDBDB]"
         />
-        <label htmlFor="Phone Number" className="pb-2 font-black">
+        <p
+          className={`${validationObject.email.errors ? 'font-bold text-[#BE1B1B]' : 'hidden'}`}
+        >
+          {validationObject.email.errorMessage}
+        </p>
+        <label htmlFor="phoneNumber" className="pb-2 font-black">
           Phone Number
         </label>
         <input
-          id="Phone Number"
+          id="phoneNumber"
+          name="phoneNumber"
           type="text"
           required
+          placeholder="123-45-678"
+          value={validationObject.phone.value}
+          onChange={handlePhoneChange}
+          onBlur={validatePhone}
           className="mb-[13px] min-h-[50px] w-full border-[2px] border-[#DBDBDB]"
         />
-        <label htmlFor="help" className="pb-2 font-black">
+        <p
+          className={`${validationObject.phone.errors ? 'font-bold text-[#BE1B1B]' : 'hidden'}`}
+        >
+          {validationObject.phone.errorMessage}
+        </p>
+        <label htmlFor="body" className="pb-2 font-black">
           How Can We Help? *
         </label>
         <textarea
-          id="help"
+          id="body"
+          name="body"
+          placeholder="I'd like to talk about..."
           required
-          className="mb-[13px] min-h-[190px] w-full border-[2px] border-[#DBDBDB] p-1"
+          className="mb-[13px] min-h-[190px] w-full resize-none border-[2px] border-[#DBDBDB] p-1"
         />
 
-        <div
-          className={`${lato.className} mb-[70px] flex min-h-[40px] min-w-[135px] max-w-[135px] items-center justify-center rounded-full bg-gradient-to-r from-[#072c58] from-5% to-[#034998] to-80%  text-[16px] font-[700] leading-[21px] text-white `}
-        >
-          SUBMIT
-        </div>
-      </span>
+        <input
+          type="submit"
+          value={'SUBMIT'}
+          disabled={validationObject.email.errors}
+          className={`${lato.className} mb-[70px] flex min-h-[40px] min-w-[135px] max-w-[135px]  items-center ${validationObject.email.errors ? 'bg-blue-500/30' : 'cursor-pointer bg-gradient-to-r from-[#072c58] from-5% to-[#034998] to-80%'} justify-center rounded-full   text-[16px] font-[700] leading-[21px] text-white `}
+        />
+      </form>
     </section>
   );
 };
