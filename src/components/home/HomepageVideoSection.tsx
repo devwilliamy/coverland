e87ea64974df33e653d095722ff39304f65529e7
William Yang <dev.william.y@icarcover.com>
2024-03-26 16:56:29 -0700

    Trying regular video player here

--- src/components/home/HomepageVideoSection.tsx
+++ src/components/home/HomepageVideoSection.tsx
@@ -1,10 +1,9 @@
 import dynamic from 'next/dynamic';
-import SquareVideo from '@/videos/Coverland_Square.mp4';
 import NewVideo from '@/videos/Cinematic Car Cover Ads_FINAL.mp4';
-import SquareThumbnail from '@/video/Thumbnail_Square.webp';
 import NewVideoThumb from '@/video/Thumbnail.webp';
 import { Skeleton } from '@/components/ui/skeleton';
-
+import { Suspense } from 'react';
+import { list } from '@vercel/blob';
 const ProductVideo = dynamic(() => import('@/components/PDP/ProductVideo'), {
   loading: () => (
     <div className="flex h-full">
@@ -12,6 +11,33 @@ const ProductVideo = dynamic(() => import('@/components/PDP/ProductVideo'), {
     </div>
   ),
 });
+
+async function VideoComponent({ fileName }) {
+  const pathname = 'videos/Cinematic Car Cover Ads_FINAL.mp4';
+  const { blobs } = await list({ prefix: fileName });
+  const video = blobs.find((blob) => blob.pathname === pathname);
+  // console.log('Blods:', blobs);
+  // console.log('Video:', video);
+  const { url } = video;
+  // console.log('URL:', url);
+
+  return (
+    <video
+      controls
+      preload="none"
+      aria-label="Video player"
+      // autoPlay
+      muted
+      // loop
+      poster="/video/Thumbnail.webp"
+      width={400}
+      height={230}
+    >
+      <source src={url} type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  );
+}
 
 export default function HomepageVideoSection() {
   return (
@@ -24,11 +50,9 @@ export default function HomepageVideoSection() {
         Perfect Solution for All Weather
       </p>
       <span className="flex w-full flex-col items-center bg-[#1A1A1A] px-[18px] pt-[45px] lg:px-[150px] lg:pt-[92px]">
-        <ProductVideo
-          src={NewVideo}
-          imgSrc={NewVideoThumb}
-          aspectRatio="16 / 9"
-        />
+        <Suspense fallback={<p>Loading video...</p>}>
+          <VideoComponent fileName="videos/" />
+        </Suspense>
         <button className="my-[30px] flex min-h-[48px] w-full max-w-[214px]  flex-col items-center justify-center rounded-[4px] bg-[#BE1B1B] px-[40px] py-[15px] text-[16px] font-black text-white ">
           <a href="/car-covers">Shop Now</a>
         </button>
