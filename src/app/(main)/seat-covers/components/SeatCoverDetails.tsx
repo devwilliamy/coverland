deaa8ec5b6e68893d1eee429d7a6c7458c750031
George Anumba <dev.george@icarcover.com>
2024-04-02 11:52:23 -0700

    - Restricted banner to only ford F-150 page - Removed submodel text per calvins request

--- src/app/(main)/seat-covers/components/SeatCoverDetails.tsx
+++ src/app/(main)/seat-covers/components/SeatCoverDetails.tsx
@@ -5,13 +5,24 @@ import StayNewSection from './StayNewSection';
 import SafetyFirstSection from './SafetyFirstSection';
 import EnhancedPerformanceSection from './EnhancedPerformanceSection';
 import WarrantySection from '@/components/PDP/components/WarrantySection';
+import { useParams } from 'next/navigation';
 
 export default function SeatCoverDetails() {
+  const params = useParams();
+  const isFordF1502015 =
+    params?.make === 'ford' &&
+    params?.model === 'f-150' &&
+    params?.year === '2015-2024';
+  console.log(params);
+
   return (
-    <div className='flex w-full flex-col'>
-      <div className="flex w-full lg:hidden">
-        <DetailsTabHeader submodel="Super Crew Cap" />
-      </div>
+    <div className="flex w-full flex-col">
+      {isFordF1502015 && (
+        <div className="flex w-full lg:hidden">
+          <DetailsTabHeader />
+        </div>
+      )}
+
       <ElevateComfortSection />
       <StayNewSection />
       <SafetyFirstSection />
