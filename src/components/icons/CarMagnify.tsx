f28f04f5372b0374e94690990578881eae7647d0
George Anumba <dev.george@icarcover.com>
2024-04-02 17:41:23 -0700

    - Adding width and height to Car Magnify icon

--- src/components/icons/CarMagnify.tsx
+++ src/components/icons/CarMagnify.tsx
@@ -7,6 +7,8 @@ export default function CarMagnify() {
       data-name="Layer 2"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 129.53 57.68"
+      width={67}
+      height={30}
     >
       <g id="Layer_1-2" data-name="Layer 1">
         <g>
