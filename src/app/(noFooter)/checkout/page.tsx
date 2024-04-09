53baf320ee80affee211b1503b17fcaf6e9fb450
William Yang <dev.william.y@icarcover.com>
2024-04-09 16:55:21 -0700

    Fixed some warnings

--- src/app/(noFooter)/checkout/page.tsx
+++ src/app/(noFooter)/checkout/page.tsx
@@ -1,25 +1,17 @@
 'use client';
-import { useState } from 'react';
 import { useCheckoutViewedGoogleTag } from '@/hooks/useGoogleTagDataLayer';
-import YourCartSection from '@/components/checkout/YourCart';
-import Shipping from '@/components/checkout/Shipping';
 import { Elements } from '@stripe/react-stripe-js';
 import useGetStripePaymentIntent from '@/hooks/useGetStripePaymentIntent';
-import { loadStripe } from '@stripe/stripe-js';
-import Payment from '@/components/checkout/Payment';
-import CheckoutSummarySection from '@/components/checkout/CheckoutSummarySection';
-import { useCartContext } from '@/providers/CartProvider';
-import OrderReview from '@/components/checkout/OrderReview';
+import { Appearance, loadStripe } from '@stripe/stripe-js';
 import { useMediaQuery } from '@mantine/hooks';
 import MobileCheckout from '@/app/(main)/[productType]/components/MobileCheckout';
-import { CheckoutStep } from '@/lib/types/checkout';
 import {
   CheckoutProvider,
   useCheckoutContext,
 } from '@/contexts/CheckoutContext';
 import DesktopCheckout from '@/components/checkout/DesktopCheckout';
 
-const appearance = {
+const appearance: Appearance = {
   theme: 'stripe',
   variables: {
     colorPrimary: '#ed5f74',
@@ -33,32 +25,8 @@ const stripePromise = loadStripe(
 );
 
 function CheckoutPage() {
-  // const [currentStep, setCurrentStep] = useState(CheckoutStep.CART);
-  const { currentStep, nextStep, prevStep } = useCheckoutContext();
-  console.log('CurrentStep:', { currentStep, nextStep, prevStep });
-
   const { clientSecret } = useGetStripePaymentIntent();
-  const {
-    cartItems,
-    getTotalPrice,
-    getOrderSubtotal,
-    getTotalDiscountPrice,
-    getTotalCartQuantity,
-    clearLocalStorageCart,
-  } = useCartContext();
   const isMobile = useMediaQuery('(max-width: 1024px)');
-
-  const totalMsrpPrice = getTotalPrice().toFixed(2) as unknown as number;
-  const totalDiscountedPrice = getTotalDiscountPrice().toFixed(
-    2
-  ) as unknown as number;
-  const orderSubtotal = getOrderSubtotal().toFixed(2) as unknown as number;
-  const cartQuantity = getTotalCartQuantity();
-
-  const options = {
-    clientSecret,
-    appearance,
-  };
 
   useCheckoutViewedGoogleTag();
 
@@ -66,17 +34,22 @@ function CheckoutPage() {
     <div>
       {clientSecret && (
         <CheckoutProvider>
-          <Elements stripe={stripePromise} options={options}>
-            <>
-              {isMobile ? (
-                <MobileCheckout />
-              ) : (
-                <>
-                  <DesktopCheckout />
-                </>
-              )}
-            </>
-          </Elements>
+          {clientSecret && (
+            <Elements
+              stripe={stripePromise}
+              options={{ clientSecret, appearance }}
+            >
+              <>
+                {isMobile ? (
+                  <MobileCheckout />
+                ) : (
+                  <>
+                    <DesktopCheckout />
+                  </>
+                )}
+              </>
+            </Elements>
+          )}
         </CheckoutProvider>
       )}
     </div>
