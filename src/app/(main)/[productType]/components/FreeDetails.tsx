356641f6b2327676e7b7755b6e2bf39256d7d049
George Anumba <dev.george@icarcover.com>
2024-04-09 10:58:15 -0700

    - Started working on warranty tab

--- src/app/(main)/[productType]/components/FreeDetails.tsx
+++ src/app/(main)/[productType]/components/FreeDetails.tsx
@@ -13,6 +13,7 @@ import {
 import { determineDeliveryByDate } from '@/lib/utils/deliveryDateUtils';
 import {
   BoxIcon,
+  Check,
   ChevronRight,
   ShieldCheck,
   ShieldOffIcon,
@@ -33,6 +34,7 @@ type DetailItem = {
 
 export default function FreeDetails() {
   const deliveryDate = determineDeliveryByDate();
+  const iconSize = 28;
   const [timeRemaining, setTimeRemaining] = useState(calculateTimeTo2PM());
   const [currentPage, setCurrentPage] = useState<DetailItem>({
     icon: <></>,
@@ -44,6 +46,7 @@ export default function FreeDetails() {
   const params = useParams();
   const coverType = params?.coverType;
   let warrantyLength: string | number = 'Lifetime';
+  const indentStyling = 'flex flex-col gap-4 pl-[30px] pt-5';
 
   switch (coverType) {
     case 'standard':
@@ -77,7 +80,7 @@ export default function FreeDetails() {
     icon: React.JSX.Element;
     title: string;
   }) => (
-    <div className="flex items-center gap-3 font-[500] text-[#2BA45B]">
+    <div className="flex items-center gap-3 pt-10 font-[500] text-[#2BA45B]">
       {icon}
       <p className="text-[18px] leading-[18px]">{title}</p>
     </div>
@@ -93,117 +96,160 @@ export default function FreeDetails() {
     <div className="mt-2 flex max-h-[4px] min-h-[4px] min-w-[4px] max-w-[4px] rounded-full bg-black" />
   );
 
+  const BulletItem = ({ text }: { text: string }) => (
+    <div className="grid w-full grid-cols-[4px_auto] gap-2 ">
+      <TitleDot />
+      <p className="flex font-[500]">{text}</p>
+    </div>
+  );
+
+  const CheckItem = ({
+    text,
+    gap = 2,
+    description,
+  }: {
+    text: string;
+    gap?: number;
+    description?: string;
+  }) => (
+    <div className={`flex gap-${gap}`}>
+      <Check
+        className="text-[#2BA45B]"
+        strokeWidth={4}
+        strokeLinecap="square"
+        strokeLinejoin="miter"
+      />
+      <div></div>
+      <p className="font-[500]">{text}</p>
+    </div>
+  );
+
   const ShippingInformation = () => (
-    <div className="flex flex-col px-5 pt-10 ">
+    <div className="flex flex-col px-5 ">
       <IncludedDetailTitle
-        icon={<BoxIcon size={28} />}
+        icon={<BoxIcon size={iconSize} />}
         title={'Free Shipping'}
       />
-      <div className="flex flex-col pl-[35px] pt-5">
+      <div className={indentStyling}>
         {[
           'We offer free shipping to all 48 contiguous United States via UPS.',
           'Orders placed before 2 PM PT (Monday-Saturday) are shipped the same day and delivered within 5 business days. ',
           'We also ship to Hawaii, Alaska, Puerto Rico, Virgin Island, Guam, APO, FPO for $49.99 (3-6 business days) via USPS.',
           'All items are in stock and ready to ship immediately.',
         ].map((text) => (
-          <div className="grid w-full grid-cols-[4px_auto] gap-2 ">
-            <TitleDot />
-            <p className="flex">{text}</p>
-          </div>
+          <BulletItem text={text} />
         ))}
       </div>
-      <div className=" pt-10">
-        <IncludedDetailTitle
-          icon={<BsGlobeAmericas className=" h-[28px] w-[28px]" />}
-          title={'Free Shipping'}
+      <IncludedDetailTitle
+        icon={<BsGlobeAmericas className=" h-[28px] w-[28px]" />}
+        title={'International Shipping'}
+      />
+      <div className={indentStyling}>
+        <BulletItem
+          text="We ship worldwide. You can view your shipping options during
+            checkout."
         />
-        <div className="grid w-full grid-cols-[4px_auto] gap-2 pl-[35px] pt-5 ">
-          <TitleDot />
-          <p className="flex">
-            We ship worldwide. You can view your shipping options during
-            checkout.
-          </p>
-        </div>
       </div>
     </div>
   );
 
   const ServiceGurantee = () => (
-    <div className="flex flex-col px-5 pt-10 ">
+    <div className="flex flex-col px-5 ">
       <IncludedDetailTitle
-        icon={<BoxIcon size={28} />}
+        icon={<ShieldCheck size={iconSize} />}
         title={'Free Returns'}
       />
-      <div className="grid w-full grid-cols-[4px_auto] gap-2 pl-[35px] pt-5">
-        <TitleDot />
-        <p className="flex">
-          Return products within 30 days for a full refund or exchange. We
-          provide the return shipping label. Contact us to start the process.
-        </p>
+      <div className={indentStyling}>
+        <BulletItem
+          text="Return products within 30 days for a full refund or exchange. We
+          provide the return shipping label. Contact us to start the process."
+        />
       </div>
+
       <a
         href="/contact"
         className="pl-[60px] pt-7 text-[14px] leading-[14px] text-[#4C8EA8]"
       >
         Contact Us
       </a>
-      <div className=" pt-10">
-        <IncludedDetailTitle
-          icon={<BsGlobeAmericas className=" h-[28px] w-[28px]" />}
-          title={'Purchase Protection'}
-        />
-        <div className="grid w-full grid-cols-[4px_auto] gap-2 pl-[35px] pt-5 ">
-          <TitleDot />
-          <p className="flex">
-            We ship worldwide. You can view your shipping options during
-            checkout.
-          </p>
-        </div>
+
+      <IncludedDetailTitle
+        icon={<ShieldCheck className=" h-[28px] w-[28px]" />}
+        title={'Purchase Protection'}
+      />
+      <div className={indentStyling}>
+        <BulletItem text="We ship worldwide. You can view your shipping options during checkout." />
       </div>
       <Separator className="my-10" />
       <NotIncludedTitle title="International Orders" />
-      <div className="grid w-full grid-cols-[4px_auto] gap-2 pb-[80px] pl-[35px] pt-5 ">
-        <TitleDot />
-        <p className="flex">
-          Orders shipped outside the USA are not eligible for free returns. You
-          cover the return shipping, and we refund the product cost only,
-          excluding shipping.
-        </p>
+      <div className={indentStyling + ' pb-10'}>
+        <BulletItem
+          text="Orders shipped outside the USA are not eligible for free returns. You
+        cover the return shipping, and we refund the product cost only,
+        excluding shipping."
+        />
       </div>
     </div>
   );
 
   const Warranty = () => (
-    <div className="flex flex-col px-5 pt-10 ">
+    <div className="flex flex-col px-5 ">
       <IncludedDetailTitle
-        icon={<BoxIcon size={28} />}
-        title={'Free Returns'}
+        icon={<ShieldCheck size={iconSize} />}
+        title={'Lifetime Warranty Policy'}
       />
-      <div className="grid w-full grid-cols-[4px_auto] gap-2 pl-[35px] pt-5">
-        <TitleDot />
-        <p className="flex">
-          Return products within 30 days for a full refund or exchange. We
-          provide the return shipping label. Contact us to start the process.
-        </p>
+
+      <div className={indentStyling}>
+        <BulletItem text="Our warranty covers your product for a lifetime against various issues, including:" />
+        <div className="flex flex-col gap-1">
+          {[
+            'Normal Wear and Tear',
+            'Weather-Related Damages',
+            'Ripping and Tears',
+          ].map((text) => (
+            <CheckItem text={text} />
+          ))}
+        </div>
       </div>
-      <a
-        href="/contact"
-        className="pl-[60px] pt-7 text-[14px] leading-[14px] text-[#4C8EA8]"
-      >
-        Contact Us
-      </a>
-      <div className=" pt-10">
+
+      <div className="pt-10">
         <IncludedDetailTitle
           icon={<BsGlobeAmericas className=" h-[28px] w-[28px]" />}
           title={'Purchase Protection'}
         />
-        <div className="grid w-full grid-cols-[4px_auto] gap-2 pl-[35px] pt-5 ">
-          <TitleDot />
-          <p className="flex">
-            We ship worldwide. You can view your shipping options during
-            checkout.
-          </p>
+        <div className={indentStyling}>
+          <BulletItem
+            text="We ship worldwide. You can view your shipping options during
+        checkout."
+          />
         </div>
+      </div>
+
+      <Separator className="my-10" />
+      <NotIncludedTitle title="International Orders" />
+      <div className={indentStyling + ' pb-10'}>
+        {[
+          'Damage due to improper use or installation.',
+          'Alterations or modifications made to the car cover.',
+          'Damage caused by accidents, abuse, or neglect.',
+        ].map((text) => (
+          <BulletItem text={text} />
+        ))}
+      </div>
+      <Separator className="my-10" />
+      <p className="text-[18px] leading-[18px] text-[#2BA45B]">
+        How to Claim Your Warranty in 3 Steps:
+      </p>
+      <div className="pt-10">
+        {[
+          'Step 1: Submit a claim',
+          'Step 2: Processing',
+          'Step 3: Replacement',
+        ].map((text) => (
+          <div className="">
+            <CheckItem text={text} gap={5} />
+          </div>
+        ))}
       </div>
     </div>
   );
@@ -293,7 +339,7 @@ export default function FreeDetails() {
         >
           <SheetHeader className="sticky top-0 flex w-full flex-col bg-white px-4  pt-4 text-[20px] leading-[20px]">
             <SheetClose className="absolute right-3 top-[0] mr-4 mt-[20px] flex rounded-full bg-[#F0F0F099] p-1.5">
-              <X size={28} />
+              <X size={iconSize} />
             </SheetClose>
             <div className="pb-[24px] pt-[48px] text-[20px] font-[500] leading-[20px] ">
               {currentPage.headerText}
