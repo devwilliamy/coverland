ca259cf8179932c448d03f6b1bf616bea5174f69
William Yang <dev.william.y@icarcover.com>
2024-04-25 14:58:46 -0700

    Removing commas when car isn't selected

--- src/app/(main)/[productType]/components/ProductContent.tsx
+++ src/app/(main)/[productType]/components/ProductContent.tsx
@@ -110,7 +110,7 @@ export function ProductContent({
   const ProductTitle = () => (
     <h1 className="mt-[24px] text-[24px] font-[900] leading-[27px] text-[#1A1A1A] lg:mt-0 lg:text-[28px] lg:leading-[30px] ">
       {!make && !model ? (
-        <>Waterproof, Outdoor, Custom-Fit {`${productType} `}</>
+        <>Waterproof Outdoor Custom-Fit {`${productType} `}</>
       ) : (
         <>
           {make && `${deslugify(make as string)} `}
