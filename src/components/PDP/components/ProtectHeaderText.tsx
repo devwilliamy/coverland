d954ad193d6cb9adf7879316113874dacac162df
George Anumba <dev.george@icarcover.com>
2024-05-30 11:57:25 -0700

    - Working on Elevate style change

--- src/components/PDP/components/ProtectHeaderText.tsx
+++ src/components/PDP/components/ProtectHeaderText.tsx
@@ -5,20 +5,13 @@ import { useMediaQuery } from '@mantine/hooks';
 export default function ProtectHeaderText() {
   const { make, model, productType } = useDetermineType();
   const isMobile = useMediaQuery('(max-width: 1023px)');
-  let productTypeFormatted = productType;
-  if (productType === 'truck-covers') {
-    productTypeFormatted = 'Truck';
-  } else if (productType === 'suv-covers') {
-    productTypeFormatted = productType.toUpperCase();
-    productTypeFormatted = productType?.slice(0, 3);
-  } else {
-    productTypeFormatted = productType?.slice(0, 3);
-  }
-  const protectText = model ?? make ?? productTypeFormatted;
+  let vehicleType = productType;
+  console.log(productType);
+  vehicleType?.split('-')[0];
 
   return (
     <p className="w-full text-[26px] font-[500] leading-[26px] text-white lg:p-[6px] lg:pt-[28px] lg:text-[45px] lg:leading-[32px]">
-      Protect your <span className={`capitalize`}>{protectText}</span> now
+      Best Cover for <span className={`capitalize`}>{vehicleType}</span>s
     </p>
   );
 }
