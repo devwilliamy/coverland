01af274fccfb5a3fdcc5a5aefdc9c6c52dd65cc6
coverlandjohnlee <john.l.coverland@gmail.com>
2024-06-10 10:43:50 -0700

    OrderList and OrderItem components

---
+++ src/app/(main)/profile/orders/components/OrderItem.tsx
@@ -0,0 +1,47 @@
+// components/OrderItemCard.tsx
+import Image from 'next/image';
+// import { TInitialOrderItemsDataDB } from '@/types';
+import { TInitialOrderItemsDataDB, TInitialProductDataDB } from '@/lib/db/profile/ordersHistory';
+
+interface OrderItemCardProps {
+  item: TInitialOrderItemsDataDB;
+}
+
+const OrderItemCard: React.FC<OrderItemCardProps> = ({ item }) => {
+  return (
+    <li key={item.id}>
+      <div className="md:m-0 md:flex justify-end">
+        <div className="mt-4 mb-4 md:w-2/5 md:mt-2 md:mb-2">
+          <div className="md:flex justify-end">
+            <Image
+              className="bg-gray-100 p-[6.5px]"
+              src={item.product?.feature || ''}
+              width={155}
+              height={155}
+              alt="Picture of the Order Item"
+            />
+          </div>
+        </div>
+        <div className="mt-4 mb-4 md:mt-2 md:mb-2 md:ml-5 md:w-3/5 pt-0 md:max-w-[325px]">
+          <div className="mb-1 text-base font-bold md:text-base">
+            {`${item.product?.display_id}™ ${item.product?.type}`}
+          </div>
+          <div className="mb-1 text-base font-normal text-[#707070] md:text-base">
+            Vehicle: {item.product?.make} {item.product?.model} {item.product?.year_generation} {item.product?.submodel1}
+          </div>
+          <div className="mb-1 text-base font-normal text-[#707070] md:text-base">
+            Color: {item.product?.display_color}
+          </div>
+          <div className="mb-1 text-base font-normal text-[#707070] md:text-base">
+            Qty: {item.quantity} @ ${item.product?.msrp}
+          </div>
+          <div className="text-base font-normal text-[#707070] md:text-base">
+            {item.price}
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default OrderItemCard;
