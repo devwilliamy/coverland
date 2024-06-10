f9be84eafd9b6c83c32bfc6b3c69caeb063fea8b
coverlandjohnlee <john.l.coverland@gmail.com>
2024-06-10 09:57:37 -0700

    mobile css

--- src/app/(main)/profile/orders/page.tsx
+++ src/app/(main)/profile/orders/page.tsx
@@ -45,11 +45,11 @@ export default async function Orders() {
           View, Manage and track orders
         </p>
       </div>
-      <Card className="p-2 mb-4">
-        <CardHeader className="text-xl md:text-xl font-bold p-4 pt-2 pb-2 lg:p-6">Recent Orders</CardHeader>
+      <Card className="p-2 m-1 md:m-0 md:mb-4">
+        <CardHeader className="text-xl md:text-xl font-bold p-2 pt-4 pb-4 md:p-4">Recent Orders</CardHeader>
         {orders.map((order: TInitialOrdersDataDB) => (
-          <div key={order.id} className="md:m-2 md:flex justify-between border-t p-4">
-            <div className="md:w-2/5">
+          <div key={order.id} className="md:m-2 md:flex justify-between border-t p-4 pl-2">
+            <div className="mt-6 mb-8 md:mt-4 md:mb-4 md:w-2/5">
               <div className="flex justify-left gap-2">
                 <span className="false mb-1 text-base font-normal text-[#707070] md:text-base min-w-[110px]">Order Number</span><span className="font-semibold">#{order.id}</span>
               </div>
@@ -57,44 +57,44 @@ export default async function Orders() {
                 <span className="false mb-1 text-base font-normal text-[#707070] md:text-base min-w-[110px]">Order Date</span><span className="font-semibold">{order.payment_date}</span>
               </div>
               <div className="flex justify-left gap-2">
-                <span className="false mb-1 text-base font-normal text-[#707070] md:text-base min-w-[110px]">Total</span><span className="font-semibold">{order.total_amount}</span>
+                <span className="false mmb-1 text-base font-normal text-[#707070] md:text-base min-w-[110px]">Total</span><span className="font-semibold">{order.total_amount}</span>
               </div>
             </div>
             <ul className="md:w-3/5">
               {order.items?.map((item: TInitialOrderItemsDataDB) => (
                 <li key={item.id}>
-                  <div className="m-2 md:flex justify-end">
-                    <div className="md:w-2/5">
+                  <div className="md:m-0 md:flex justify-end">
+                    <div className="mt-4 mb-4 md:w-2/5 md:mt-2 md:mb-2">
                       <div className="md:flex justify-end">
                         <Image
                           className="bg-gray-100 p-[6.5px]"
                           src={item.product?.feature}
-                          width={150}
-                          height={150}
+                          width={155}
+                          height={155}
                           alt="Picture of the Order Item"
                         />
                       </div>
                     </div>
-                    <div className="md:ml-5 md:w-3/5 pt-0 md:max-w-[325px]">
+                    <div className="mt-4 mb-4 md:mt-2 md:mb-2 md:ml-5 md:w-3/5 pt-0 md:max-w-[325px]">
                       <div className="false mb-1 text-base font-bold md:text-base">
                         {`${item.product?.display_id}${trademarkSymbol} ${item.product?.type}`}
                       </div>
-                      {/* <div className="false mb-1 text-base font-normal text-[#707070] md:text-base">
+                      {/* <div className="mb-1 text-base font-normal text-[#707070] md:text-base">
                         {`${item.product?.display_id}${trademarkSymbol} ${item.product?.type}`}
                       </div> */}
-                      <div className="false mb-1 text-base font-normal text-[#707070] md:text-base">
+                      <div className="mb-1 text-base font-normal text-[#707070] md:text-base">
                         Vehicle: {item.product?.make} {item.product?.model}{' '}
                         {item.product?.year_generation}{' '}
                         {item.product?.submodel1}
                       </div>
-                      <div className="false mb-1 text-base font-normal text-[#707070] md:text-base">
+                      <div className="mb-1 text-base font-normal text-[#707070] md:text-base">
                         Color: {item.product?.display_color}
                       </div>
                       {/* <div>Price: {item.product?.price}</div> */}
-                      <div className="false mb-1 text-base font-normal text-[#707070] md:text-base">
+                      <div className="mb-1 text-base font-normal text-[#707070] md:text-base">
                         Qty:{item.quantity} @ ${item.product?.msrp}
                       </div>
-                      <div className="false text-base font-normal text-[#707070] md:text-base">
+                      <div className="text-base font-normal text-[#707070] md:text-base">
                         {item.price}
                       </div>
                     </div>
