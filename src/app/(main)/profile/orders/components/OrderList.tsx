8f689def2808041a1aedd505a1031a2c702541cc
coverlandjohnlee <john.l.coverland@gmail.com>
2024-06-10 10:43:50 -0700

    OrderList and OrderItem components

---
+++ src/app/(main)/profile/orders/components/OrderList.tsx
@@ -0,0 +1,48 @@
+// components/OrderList.tsx
+import OrderItem from './OrderItem';
+import { Card, CardHeader } from '@/components/ui/card'; // Import your Card components
+// import { TInitialOrdersDataDB, TInitialOrderItemsDataDB } from '@/types';
+import { TInitialOrdersDataDB, TInitialOrderItemsDataDB } from '@/lib/db/profile/ordersHistory';
+
+interface OrderListProps {
+  orders: TInitialOrdersDataDB[];
+}
+
+const OrderList: React.FC<OrderListProps> = ({ orders }) => {
+  return (
+    <>
+      <div className="pl-5 md:pl-3">
+        <h1 className="mt-4 text-xl md:text-2xl font-bold">My Orders</h1>
+        <p className="mb-4 text-sm text-gray-500">View, Manage and track orders</p>
+      </div>
+      <Card className="p-2 m-3 md:m-1 md:m-0 md:mb-4">
+        <CardHeader className="text-xl md:text-xl font-bold p-2 pt-4 pb-4 md:p-4">Recent Orders</CardHeader>
+        {orders.map((order) => (
+          <div key={order.id} className="md:m-2 md:flex justify-between border-t p-4 pl-2">
+            <div className="mt-6 mb-8 md:mt-4 md:mb-4 md:w-2/5">
+              <div className="flex justify-left gap-2">
+                <span className="mb-1 text-base font-normal text-[#707070] md:text-base min-w-[110px]">Order Number</span>
+                <span className="font-semibold">#{order.id}</span>
+              </div>
+              <div className="flex justify-left gap-2">
+                <span className="mb-1 text-base font-normal text-[#707070] md:text-base min-w-[110px]">Order Date</span>
+                <span className="font-semibold">{order.payment_date}</span>
+              </div>
+              <div className="flex justify-left gap-2">
+                <span className="mb-1 text-base font-normal text-[#707070] md:text-base min-w-[110px]">Total</span>
+                <span className="font-semibold">{order.total_amount}</span>
+              </div>
+            </div>
+            <ul className="md:w-3/5">
+              {order.items?.map((item) => (
+                <OrderItem key={item.id} item={item} />
+              ))}
+            </ul>
+          </div>
+        ))}
+      </Card>
+    </>
+  );
+};
+
+export default OrderList;
--- src/app/(main)/profile/orders/page.tsx
+++ src/app/(main)/profile/orders/page.tsx
@@ -11,114 +11,23 @@ import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adap
 import { SupabaseClient } from '@supabase/supabase-js';
 import { cookies } from 'next/headers';
 import { createSupabaseServerClient } from '@/lib/db/supabaseClients';
-import Image from 'next/image';
-import OrderItemCard from '@/components/profile/orders/OrderItemCard';
 
-import {
-  Card,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-  CardDescription,
-  CardContent,
-} from '@/components/ui/card';
 
-export default async function Orders() {
-  const orders: TInitialOrdersDataDB[] = await fetchUserRecentOrders(4);
+import OrderList from './components/OrderList';
+import { TInitialOrdersDataDB } from '@/types';
 
-  const cookieStore: ReadonlyRequestCookies = cookies();
-  const supabase: SupabaseClient = createSupabaseServerClient(cookieStore);
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+const orders: TInitialOrdersDataDB[] = await fetchUserRecentOrders(4);
 
-  const trademarkSymbol = '\u2122';
+const cookieStore: ReadonlyRequestCookies = cookies();
+const supabase: SupabaseClient = createSupabaseServerClient(cookieStore);
+const {
+  data: { user },
+} = await supabase.auth.getUser();
 
-  // console.log('items', orders);
+const trademarkSymbol = '\u2122';
 
-  return (
-    <div className="">
-      {/* <OrderItemCard/> */}
-      <div className="pl-5 md:pl-3">
-        <h1 className="mt-4 text-xl font-bold md:text-2xl">My Orders</h1>
-        <p className="mb-4 text-sm text-gray-500">
-          View, Manage and track orders
-        </p>
-      </div>
-      <Card className="m-1 p-2 md:m-0 md:mb-4">
-        <CardHeader className="p-2 pb-4 pt-4 text-xl font-bold md:p-4 md:text-xl">
-          Recent Orders
-        </CardHeader>
-        {orders.map((order: TInitialOrdersDataDB) => (
-          <div
-            key={order.id}
-            className="justify-between border-t p-4 pl-2 md:m-2 md:flex"
-          >
-            <div className="mb-8 mt-6 md:mb-4 md:mt-4 md:w-2/5">
-              <div className="justify-left flex gap-2">
-                <span className="mb-1 min-w-[110px] text-base font-normal text-gray-500 md:text-base">
-                  Order Number
-                </span>
-                <span className="font-semibold">#{order.id}</span>
-              </div>
-              <div className="justify-left flex gap-2">
-                <span className="mb-1 min-w-[110px] text-base font-normal text-gray-500 md:text-base">
-                  Order Date
-                </span>
-                <span className="font-semibold">{order.payment_date}</span>
-              </div>
-              <div className="justify-left flex gap-2">
-                <span className="mmb-1 min-w-[110px] text-base font-normal text-gray-500 md:text-base">
-                  Total
-                </span>
-                <span className="font-semibold">{order.total_amount}</span>
-              </div>
-            </div>
-            <ul className="md:w-3/5">
-              {order.items?.map((item: TInitialOrderItemsDataDB) => (
-                <li key={item.id}>
-                  <div className="justify-end md:m-0 md:flex">
-                    <div className="mb-4 mt-4 md:mb-2 md:mt-2 md:w-2/5">
-                      <div className="justify-end md:flex">
-                        <Image
-                          className="bg-gray-100 p-[6.5px]"
-                          src={item.product?.feature}
-                          width={155}
-                          height={155}
-                          alt="Picture of the Order Item"
-                        />
-                      </div>
-                    </div>
-                    <div className="mb-4 mt-4 pt-0 md:mb-2 md:ml-5 md:mt-2 md:w-3/5 md:max-w-[325px]">
-                      <div className="mb-1 text-base font-bold md:text-base">
-                        {`${item.product?.display_id}${trademarkSymbol} ${item.product?.type}`}
-                      </div>
-                      {/* <div className="mb-1 text-base font-normal text-[#707070] md:text-base">
-                        {`${item.product?.display_id}${trademarkSymbol} ${item.product?.type}`}
-                      </div> */}
-                      <div className="mb-1 text-base font-normal text-[#707070] md:text-base">
-                        Vehicle: {item.product?.make} {item.product?.model}{' '}
-                        {item.product?.year_generation}{' '}
-                        {item.product?.submodel1}
-                      </div>
-                      <div className="mb-1 text-base font-normal text-[#707070] md:text-base">
-                        Color: {item.product?.display_color}
-                      </div>
-                      {/* <div>Price: {item.product?.price}</div> */}
-                      <div className="mb-1 text-base font-normal text-[#707070] md:text-base">
-                        Qty:{item.quantity} @ ${item.product?.msrp}
-                      </div>
-                      <div className="text-base font-normal text-[#707070] md:text-base">
-                        {item.price}
-                      </div>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </Card>
-    </div>
-  );
-}
+const OrdersPage: React.FC = () => {
+  return <OrderList orders={orders} />;
+};
+
+export default OrdersPage;
