49b5ebcf3529da41ebd097425587564f90c8a532
coverlandjohnlee <john.l.coverland@gmail.com>
2024-06-13 09:58:51 -0700

    redirects to login if user is not logged in from their orders page

--- src/app/(main)/profile/orders/page.tsx
+++ src/app/(main)/profile/orders/page.tsx
@@ -10,6 +10,7 @@ import {
 import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
 import { SupabaseClient } from '@supabase/supabase-js';
 import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 import { createSupabaseServerClient } from '@/lib/db/supabaseClients';
 
 
@@ -17,6 +18,16 @@ import OrderList from './components/OrderList';
 // import { TInitialOrdersDataDB } from '@/types';
 
 const OrdersPage = async () => {
+  const cookieStore: ReadonlyRequestCookies = cookies();
+  const supabase: SupabaseClient = createSupabaseServerClient(cookieStore);
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect('/login');
+  }
+
   const orders: TInitialOrdersDataDB[] = await fetchUserRecentOrders(4);
 
   return <OrderList orders={orders} />;
