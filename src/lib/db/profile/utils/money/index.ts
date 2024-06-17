e5ca8d550cdab9fade5f42f3d6ef0157b3398607
coverlandjohnlee <john.l.coverland@gmail.com>
2024-06-17 12:12:21 -0700

    types formatMoney util function

--- src/lib/db/profile/utils/money/index.ts
+++ src/lib/db/profile/utils/money/index.ts
@@ -1,18 +1,10 @@
 /**
  * "319.9" => "$319.90"
  *
- * @param value any (expects number, or string however)
- * @returns string formatted as $XX.XX
+ * @param value number | string
+ * @returns string formatted as '$XX.XX'
  */
-export function formatMoney(value) {
-  // Ensure the value is a number
-  if (isNaN(value)) {
-    throw new Error('Invalid number');
-  }
-
-  const numberValue = parseFloat(value);
-
-  const formattedValue = numberValue.toFixed(2);
-
-  return `$${formattedValue}`;
+export function formatMoney(value: number | string): string {
+  const numberValue = typeof value === 'string' ? parseFloat(value) : value;
+  return `$${numberValue.toFixed(2)}`;
 }
