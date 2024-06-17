3f19e437e450328e5180d3491e426402c7679e49
coverlandjohnlee <john.l.coverland@gmail.com>
2024-06-17 09:22:48 -0700

    wraps supabase db server calls in try..catch, also formats document

--- src/lib/db/profile/ordersHistory.ts
+++ src/lib/db/profile/ordersHistory.ts
@@ -1,4 +1,8 @@
-import {  ADMIN_PANEL_ORDERS, ADMIN_PANEL_ORDER_ITEMS, ADMIN_PANEL_PRODUCTS } from '../constants/databaseTableNames';
+import {
+  ADMIN_PANEL_ORDERS,
+  ADMIN_PANEL_ORDER_ITEMS,
+  ADMIN_PANEL_PRODUCTS,
+} from '../constants/databaseTableNames';
 
 import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
 import { SupabaseClient } from '@supabase/supabase-js';
@@ -16,131 +20,158 @@ export type TInitialOrderItemsDataDB = Tables<'orderItems_table'>;
 
 // These custom types are utilized on the actual Profile > Order page and only contain columns that are necessary
 export type TOrderItemProduct = {
-    id: number;
-    name: string;
-    price: number;
+  id: number;
+  name: string;
+  price: number;
 };
 
 export type TOrderItem = {
-    id: number;
-    order_id: number;
-    product_id: number;
-    quantity: number;
-    product?: TInitialProductDataDB; // Optional because it will be added after fetching
+  id: number;
+  order_id: number;
+  product_id: number;
+  quantity: number;
+  product?: TInitialProductDataDB; // Optional because it will be added after fetching
 };
 
 export type TUserOrders = {
-    id: number;
-    payment_date: string;
-    total_amount: number;
-    items?: TInitialOrderItemsDataDB[]; // Optional because it will be added after fetching
+  id: number;
+  payment_date: string;
+  total_amount: number;
+  items?: TInitialOrderItemsDataDB[]; // Optional because it will be added after fetching
 }[];
 
-async function fetchUserOrders(ordersQuantity: number): Promise<TUserOrders[] | null> {
-    const cookieStore: ReadonlyRequestCookies = cookies();
-    const supabase: SupabaseClient = createSupabaseServerClient(cookieStore);
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
+async function fetchUserOrders(
+  ordersQuantity: number
+): Promise<TUserOrders[] | null> {
+  const cookieStore: ReadonlyRequestCookies = cookies();
+  const supabase: SupabaseClient = createSupabaseServerClient(cookieStore);
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-    if (!user) {
-        console.error('No user is logged in');
-        return null;
-    }
+  if (!user) {
+    console.error('No user is logged in');
+    return null;
+  }
 
-    // user object has unique UUID ('user_id') in supabase
-    const userId = user.id;
+  // user object has unique UUID ('user_id') in supabase
+  const userId = user.id;
 
+  try {
     const { data, error } = await supabase
-        .from<Order>(ADMIN_PANEL_ORDERS)
-        // if you want to grab orderItems by order ids only
-        // .select('id')
-        // this returns a inner join between orders and auth.users
-        // .select('*, users(*)')
-        .select('*')
-        // filter by logged in user_id (currently does not exist in Users table, need to add it?)
-        // .eq('user_id', userId)
-        .eq('customer_email', user?.email)
-        .in('status', ['COMPLETE', 'COMPLETED'])
-        .order('payment_date', { ascending: false }) // Order by latest date in descending order
-        .limit(ordersQuantity);
+      .from<Order>(ADMIN_PANEL_ORDERS)
+      // if you want to grab orderItems by order ids only
+      // .select('id')
+      // this returns an inner join between orders and auth.users
+      // .select('*, users(*)')
+      .select('*')
+      // filter by logged-in user_id (currently does not exist in Users table, need to add it?)
+      // .eq('user_id', userId)
+      .eq('customer_email', user?.email)
+      .in('status', ['COMPLETE', 'COMPLETED'])
+      .order('payment_date', { ascending: false }) // Order by latest date in descending order
+      .limit(ordersQuantity);
 
     if (error) {
-        console.error('Error fetching order IDs:', error);
-        return null;
-    }
-
-    return data
-}
-
-async function fetchOrderItems(orderIds: number[]): Promise<TOrderItem[] | null> {
-    const cookieStore: ReadonlyRequestCookies = cookies();
-    const supabase: SupabaseClient = createSupabaseServerClient(cookieStore);
-    const { data, error } = await supabase
-        .from<OrderItem>(ADMIN_PANEL_ORDER_ITEMS)
-        .select('*')
-        .in('order_id', orderIds);
-
-    if (error) {
-        console.error('Error fetching order items:', error);
-        return null;
+      console.error('Error fetching User Orders from supabase:', error);
+      return null;
     }
 
     return data;
+  } catch (err) {
+    console.error('Unexpected error fetching User Orders:', err);
+    return null;
+  }
 }
 
-async function fetchOrderItemProducts(productIds: number[]): Promise<TOrderItemProduct[] | null> {
-    const cookieStore: ReadonlyRequestCookies = cookies();
-    const supabase: SupabaseClient = createSupabaseServerClient(cookieStore);
+async function fetchOrderItems(
+  orderIds: number[]
+): Promise<TOrderItem[] | null> {
+  const cookieStore: ReadonlyRequestCookies = cookies();
+  const supabase: SupabaseClient = createSupabaseServerClient(cookieStore);
+  try {
     const { data, error } = await supabase
-        .from<Product>(ADMIN_PANEL_PRODUCTS)
-        .select('*')
-        .in('id', productIds);
+      .from<OrderItem>(ADMIN_PANEL_ORDER_ITEMS)
+      .select('*')
+      .in('order_id', orderIds);
 
     if (error) {
-        console.error('Error fetching products:', error);
-        return null;
+      console.error('Error fetching order items from supabase:', error);
+      return null;
     }
 
     return data;
+  } catch (err) {
+    console.error('Unexpected error fetching order items:', err);
+    return null;
+  }
 }
 
-export async function fetchUserRecentOrders(ordersQuantity: number): Promise<TUserOrders[]> {
-    // fetch recent user orders
-    const orders = await fetchUserOrders(ordersQuantity);
-    if (!orders) return;
+async function fetchOrderItemProducts(
+  productIds: number[]
+): Promise<TOrderItemProduct[] | null> {
+  const cookieStore: ReadonlyRequestCookies = cookies();
+  const supabase: SupabaseClient = createSupabaseServerClient(cookieStore);
+  try {
+    const { data, error } = await supabase
+      .from<Product>(ADMIN_PANEL_PRODUCTS)
+      .select('*')
+      .in('id', productIds);
 
-    const orderIds = orders.map(order => order.id);
+    if (error) {
+      console.error('Error fetching products from supabase:', error);
+      return null;
+    }
 
-    const orderItems = await fetchOrderItems(orderIds);
-    if (!orderItems) return;
+    return data;
+  } catch (err) {
+    console.error('Unexpected error fetching products:', err);
+    return null;
+  }
+}
 
-    const productIds = orderItems.map(item => item.product_id);
+export async function fetchUserRecentOrders(
+  ordersQuantity: number
+): Promise<TUserOrders[]> {
+  // fetch recent user orders
+  const orders = await fetchUserOrders(ordersQuantity);
+  if (!orders) return;
 
-    const products = await fetchOrderItemProducts(productIds);
-    if (!products) return;
+  const orderIds = orders.map((order) => order.id);
 
-    // Combine the data as needed
-    const userOrdersWithItemsAndProducts = orders.map(order => {
-        const items = orderItems.filter(item => item.order_id === order.id).map(item => {
-            const product = products.find(product => product.id === item.product_id);
-            return {
-                id: item.id,
-                order_id: item.order_id,
-                product_id: item.product_id,
-                quantity: item.quantity,
-                price: formatMoney(item.price) || item.price,
-                product: product
-            };
-        });
+  const orderItems = await fetchOrderItems(orderIds);
+  if (!orderItems) return;
 
+  const productIds = orderItems.map((item) => item.product_id);
+
+  const products = await fetchOrderItemProducts(productIds);
+  if (!products) return;
+
+  // Combine the data as needed
+  const userOrdersWithItemsAndProducts = orders.map((order) => {
+    const items = orderItems
+      .filter((item) => item.order_id === order.id)
+      .map((item) => {
+        const product = products.find(
+          (product) => product.id === item.product_id
+        );
         return {
-            id: order.id,
-            total_amount: formatMoney(order.total_amount) || order.total_amount,
-            payment_date: formatISODate(order.payment_date) || order.payment_date,
-            items: items
+          id: item.id,
+          order_id: item.order_id,
+          product_id: item.product_id,
+          quantity: item.quantity,
+          price: formatMoney(item.price) || item.price,
+          product: product,
         };
-    });
+      });
 
-    return userOrdersWithItemsAndProducts;
+    return {
+      id: order.id,
+      total_amount: formatMoney(order.total_amount) || order.total_amount,
+      payment_date: formatISODate(order.payment_date) || order.payment_date,
+      items: items,
+    };
+  });
+
+  return userOrdersWithItemsAndProducts;
 }
