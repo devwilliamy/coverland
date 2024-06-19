3e21942c2c19f9bcc5860aa567a8aac7456ab563
coverlandjohnlee <john.l.coverland@gmail.com>
2024-06-19 15:06:37 -0700

    sends total_original_amount and total_discount_amount as additional metadata to stripe payment intent payload to be grabbable when order is first created with new properties

--- src/app/api/stripe/payment-intent/route.ts
+++ src/app/api/stripe/payment-intent/route.ts
@@ -1,5 +1,5 @@
 import { TCartItem } from '@/lib/cart/useCart';
-import { getMsrpTotal } from '@/lib/utils/calculations';
+import { getMsrpTotal, getOrderSubtotal, getTotalDiscountPrice } from '@/lib/utils/calculations';
 import {
   convertPriceToStripeFormat,
   generateLineItemsForStripe,
@@ -17,6 +17,14 @@ const calculateOrderAmount = (items: TCartItem[]) => {
   // Calculate the order total on the server to prevent
   // people from directly manipulating the amount on the client
   return convertPriceToStripeFormat(getMsrpTotal(items));
+};
+
+const calculateOrderTotalOriginalAmount = (items: TCartItem[]) => {
+  return getOrderSubtotal(items);
+};
+
+const calculateOrderTotalDiscountAmount = (items: TCartItem[]) => {
+  return getTotalDiscountPrice(items);
 };
 
 export async function POST(request: NextRequest) {
@@ -39,6 +47,7 @@ export async function POST(request: NextRequest) {
   const veryFinalLineItem = finalLineItem.length >= 500 ? finalLineItem.slice(0, 500) : finalLineItem
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
+
     currency: 'usd',
     // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
     automatic_payment_methods: {
@@ -48,7 +57,9 @@ export async function POST(request: NextRequest) {
       orderId,
       skus: skus.join(','),
       skusWithQuantity: JSON.stringify(skusWithQuantity),
-      line_items: veryFinalLineItem
+      line_items: veryFinalLineItem,
+      total_original_amount: calculateOrderTotalOriginalAmount(items).toFixed(2).toString(), // Stripe Payment Intent API requires all metadata to be sent as a string
+      total_discount_amount: calculateOrderTotalDiscountAmount(items).toFixed(2).toString(),
     },
   });
 
