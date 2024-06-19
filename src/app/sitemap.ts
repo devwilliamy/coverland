f9766e774b0ad6697365384cc50bb78706af43c8
William Yang <dev.william.y@icarcover.com>
2024-06-19 15:03:45 -0700

    Removed suv cover and truck cover unused cover types

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -12,21 +12,21 @@ export default function sitemap(): MetadataRoute.Sitemap {
     'car-covers',
     'public/data'
   );
-  const suvCoverUrls: string[] = readJsonFilesAndGenerateUrls(
-    'suv-covers',
-    'public/data'
-  );
-  const truckCoverUrls: string[] = readJsonFilesAndGenerateUrls(
-    'truck-covers',
-    'public/data'
-  );
+  // const suvCoverUrls: string[] = readJsonFilesAndGenerateUrls(
+  //   'suv-covers',
+  //   'public/data'
+  // );
+  // const truckCoverUrls: string[] = readJsonFilesAndGenerateUrls(
+  //   'truck-covers',
+  //   'public/data'
+  // );
 
   const allUrls: string[] = [
     ...baseUrls,
     ...otherUrls,
     ...carCoverUrls,
-    ...suvCoverUrls,
-    ...truckCoverUrls,
+    // ...suvCoverUrls,
+    // ...truckCoverUrls,
   ];
   // console.log('All Urls Length: ', allUrls.length); // As of 2/28/2024: 4039 | 2/29/24: 16120
   // console.log('Other Urls Length: ', otherUrls.length); // As of 2/28/2024: 8 | 2/29/24: 8
