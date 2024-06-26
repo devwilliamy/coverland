5147269bbc4582a1b5d34c9327024ac8f7ce35dd
coverlandjohnlee <john.l.coverland@gmail.com>
2024-06-26 13:03:59 -0700

    sets up generateDynamicTemplateData function to map order data to the email template

--- src/lib/sendgrid/emails/shipping-confirmation.ts
+++ src/lib/sendgrid/emails/shipping-confirmation.ts
@@ -2,11 +2,29 @@ import sgMail, { MailDataRequired } from '@sendgrid/mail';
 sgMail.setApiKey(process.env.SENDGRID_API_KEY || '');
 const sgFromEmail = process.env.SENDGRID_FROM_EMAIL;
 const sgShippingConfirmationTemplateId = process.env.SENDGRID_SHIPPING_CONFIRMATION_EMAIL_TEMPLATE_ID;
+// import fetchUserOrderById
+
+const generateDynamicTemplateData(order) {
+    return order.map(({
+        
+    }) =>{
+        {
+            name: fullProductName || `${display_id}${trademark} ${type}`,
+            vehicle: `${make} ${model} ${year_generation} ${submodel1 || ''} ${submodel2 || ''} ${submodel3 || ''}`.trim(),
+            color: display_color,
+            quantity: quantity,
+            price: msrp,
+            total_price: formatMoneyAsNumber(msrp * quantity),
+            img_url: mainImage || feature || product.split(',')[0],
+            full_set: type === 'Seat Covers' ? isFullSet(display_set).toLowerCase() == "full" ? "Full Seat Set (Front + Rear Seat Set)": 'Front Seats (Driver +  Passenger seats)' : display_set,
+          }
+    })
+}
 
 const generateShippingConfirmationEmail(data): MailDataRequired => {
     {
         from: { email: sgFromEmail },
-        template_id: sgThankYouEmailTemplateId,   
+        template_id: sgShippingConfirmationTemplateId,   
         personalizations: [
             to: [{email: "customer@gmail.com"}],
             dynamic_template_data: data.dynamic_template_data,
@@ -20,7 +38,7 @@ export const sendShippingConfirmationEmail = async (data: MailDataRequired) => {
     } catch (error) {
         console.error(error);
 
-            // Check if err.response exists to get more details
+    // Check if err.response exists to get more details
     if (error.response) {
         console.error('Error Response:', error.response.body);
   
