22e655cb9cee4eef1fe103e761696e7811aa850a
coverlandjohnlee <john.l.coverland@gmail.com>
2024-06-26 13:35:19 -0700

    DynamicTemplateData type (all Order, Shipping, Customer, Items info consolidated into this type)

--- src/lib/sendgrid/emails/shipping-confirmation.ts
+++ src/lib/sendgrid/emails/shipping-confirmation.ts
@@ -2,9 +2,46 @@ import sgMail, { MailDataRequired } from '@sendgrid/mail';
 sgMail.setApiKey(process.env.SENDGRID_API_KEY || '');
 const sgFromEmail = process.env.SENDGRID_FROM_EMAIL;
 const sgShippingConfirmationTemplateId = process.env.SENDGRID_SHIPPING_CONFIRMATION_EMAIL_TEMPLATE_ID;
+import { formatMoneyAsNumber } from '@/lib/utils/money';
 // import fetchUserOrderById
+// import { TUserOrder, TOrderItem, TOrderItemProduct, fetchUserOrderById } from '@/lib/db/profile/ordersHistory';
+import { SHIPPING_METHOD } from '@/lib/constants';
+import { determineDeliveryByDate } from '@/lib/utils/deliveryDateUtils';
 
-const generateDynamicTemplateData(order) {
+
+const shippingConstants = {
+    shipping_method: SHIPPING_METHOD,
+    shipping_date: determineDeliveryByDate('EEE, LLL dd'), //this needs to be grabbed from somewhere (stored?)
+    delivery_fee: shipping || 0,
+  };
+
+type DynamicTemplateData = {
+    ordered_on: string;
+    shipped_on: string; //check for order.shipping_status == "shipped" and order.previous_status == "unstarted"
+    expected_delivery_on: string;
+    tracking_number: string;
+    tracking_url: string;
+    customer_name: string;
+    order_detail_page: string // url to order detail page (order.id)
+    order_items: OrderItem[];
+    shipping_info: ShippingInfo;
+  };
+
+  type ShippingInfo = {
+    full_name: string;
+    address_line1: string;
+    address_line2: string;
+    city: string;
+    state: string;
+    country: string;
+    postal_code: string;
+    shipping_method: string;
+    shipping_date: string;
+    delivery_fee: number;
+    free_delivery: boolean;
+  };
+
+const generateDynamicTemplateData(order: TUserOrder) {
     return order.map(({
         
     }) =>{
