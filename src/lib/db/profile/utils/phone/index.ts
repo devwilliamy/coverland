536f0491dec82d981fffeb3a2369b4e2e8f5a4d1
coverlandjohnlee <john.l.coverland@gmail.com>
2024-06-27 09:50:30 -0700

    changes formatPhoneNumber function to ts and also catches case where phone number is null

--- src/lib/db/profile/utils/phone/index.ts
+++ src/lib/db/profile/utils/phone/index.ts
@@ -3,23 +3,25 @@
  * 13043742252 -> (304) 374-2252
  * 3043742252 -> (304) 374-2252
  * 
- * @param phoneNumber | string of at least 10 char
- * @returns formatted phone number | string (XXX) XXX-XXXX
+ * @param phoneNumber | string of at least 10 char | null
+ * @returns formatted phone number | string (XXX) XXX-XXXX | null (if phoneNumber is null)
  */
-export function formatPhoneNumber(phoneNumber) {
+export function formatPhoneNumber(phoneNumber: string | null): string | null {
+    if (!phoneNumber) {
+        return null;
+    }
+
     // Remove all non-numeric characters
-    let cleaned = ('' + phoneNumber).replace(/\D/g, '');
-    
-    // Check if the cleaned number is at least 10 digits long
+    let cleaned: string = phoneNumber.replace(/\D/g, '');
+
     if (cleaned.length < 10) {
         throw new Error("Invalid phone number, must have at least 10 digits");
     }
-    
-    // Extract the last 10 digits of the cleaned number
-    let lastTenDigits = cleaned.slice(-10);
-    
+
+    let lastTenDigits: string = cleaned.slice(-10);
+
     // Format the last 10 digits
-    let formatted = '(' + lastTenDigits.slice(0, 3) + ') ' + lastTenDigits.slice(3, 6) + '-' + lastTenDigits.slice(6);
-    
+    let formatted: string = `(${lastTenDigits.slice(0, 3)}) ${lastTenDigits.slice(3, 6)}-${lastTenDigits.slice(6)}`;
+
     return formatted;
 }
