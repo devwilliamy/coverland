22e983d9ca13403c4df62309f30e44b3ac8fc592
coverlandjohnlee <john.l.coverland@gmail.com>
2024-07-02 13:10:36 -0700

    copies basic types over from shipping email, will reconfigure specifc type properties

--- src/lib/sendgrid/emails/delivery-confirmation.ts
+++ src/lib/sendgrid/emails/delivery-confirmation.ts
@@ -16,3 +16,44 @@ const shippingConstants = {
   shipping_date: determineDeliveryByDate('EEE, LLL dd'), //this needs to be grabbed from somewhere or pass in a starting date
   delivery_fee: shipping_fee || 0,
 };
+
+export type DynamicTemplateData = {
+  main_data: MainDeliveryEmailData;
+  order_detail_page: string; // url to order detail page (order.id)
+  order_items: OrderItem[];
+  shipping_info: ShippingInfo;
+};
+
+export type MainDeliveryEmailData = {
+  id: string;
+  ordered_on: string;
+  shipped_on: string; //check for order.shipping_status == "shipped" and order.previous_status == "unstarted"
+  expected_delivery_on: string;
+  tracking_number: string;
+  tracking_url: string;
+  customer_name: string;
+  order_detail_page: string; // url to order detail page (order.id)
+};
+
+export type OrderItem = {
+  name: string;
+  vehicle: string;
+  color: string;
+  quantity: number;
+  price: number;
+  img_url: string;
+};
+
+export type ShippingInfo = {
+  full_name: string;
+  address_line1: string;
+  address_line2: string;
+  city: string;
+  state: string;
+  country: string;
+  postal_code: string;
+  // shipping_method: string;
+  // shipping_date: string;
+  // delivery_fee: number;
+  // free_delivery: boolean;
+};
