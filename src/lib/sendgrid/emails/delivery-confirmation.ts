98451b8f63d95549e1f00a239d4c741faf37ceef
coverlandjohnlee <john.l.coverland@gmail.com>
2024-07-02 13:40:56 -0700

    generateDynamicTemplateDataFromUserOrder, note TUserOrder returned from getOrderById has string | null, while email template types expect string

--- src/lib/sendgrid/emails/delivery-confirmation.ts
+++ src/lib/sendgrid/emails/delivery-confirmation.ts
@@ -28,12 +28,12 @@ export type DynamicTemplateData = {
 export type MainDeliveryEmailData = {
   id: string;
   ordered_on: string;
-  shipped_on: string;
+  shipped_on?: string;
   delivered_on: string; //check for order.shipping_status == "delivered" and order.previous_status == "shipped"
   tracking_number: string;
   tracking_url: string;
   customer_name: string;
-  order_detail_page: string; // url to order detail page (order.id)
+  order_detail_page?: string; // url to order detail page (order.id)
 };
 
 export type OrderItem = {
@@ -57,4 +57,103 @@ export type ShippingInfo = {
   // shipping_date: string;
   // delivery_fee: number;
   // free_delivery: boolean;
+};
+
+export const generateDynamicTemplateDataFromUserOrder = (
+  order: TUserOrder
+): DynamicTemplateData => {
+  const {
+    order_id,
+    payment_date,
+    updated_at,
+    shipping_carrier,
+    shipping_status,
+    shipping_previous_status,
+    shipping_status_last_updated,
+    shipping_tracking_number,
+    customer_name,
+    items,
+  } = order;
+
+  const trademark = '\u2122';
+
+  const main_data = {
+    id: order_id,
+    ordered_on: payment_date,
+    // shipped_on: formatISODate(shipping_status_last_updated), // this is not inside _Orders table
+    delivered_on: formatISODate(shipping_status_last_updated),
+    shipping_status,
+    // expected_delivery_on: myDeliveryFunction(payment_date), // need to add a function to convert order date into estimated delivery date
+    tracking_number: shipping_tracking_number,
+    tracking_url: generateTrackingUrl(
+      shipping_tracking_number,
+      shipping_carrier
+    ),
+    customer_name,
+    // order_detail_page: `/order/${order_id}` // Example URL to order detail page
+  };
+  const {
+    shipping_address_line_1,
+    shipping_address_line_2,
+    shipping_address_city,
+    shipping_address_state,
+    shipping_address_country,
+    shipping_address_postal_code,
+  } = order;
+
+  const shipping_info = {
+    full_name: customer_name,
+    address_line1: shipping_address_line_1,
+    address_line2: shipping_address_line_2,
+    city: shipping_address_city,
+    state: shipping_address_state,
+    country: shipping_address_country,
+    postal_code: shipping_address_postal_code,
+  };
+
+  // the order items are causing an error atm //
+  /*
+            const order_items = items.map(
+        ({
+          product: {
+            fullProductName,
+            display_id,
+            type,
+            make,
+            model,
+            year_generation,
+            submodel1 = '',
+            submodel2 = '',
+            submodel3 = '',
+            display_color,
+            quantity,
+            msrp,
+            mainImage,
+            feature,
+            product,
+            display_set,
+          },
+        }) => ({
+          name: fullProductName || `${display_id} ${type}`,
+          vehicle: `${make} ${model} ${year_generation} ${submodel1 || ''} ${submodel2 || ''} ${submodel3 || ''}`.trim(),
+          color: display_color,
+          quantity: quantity,
+          price: msrp,
+          total_price: formatMoneyAsNumber(msrp * quantity),
+          img_url: mainImage || feature || product.split(',')[0],
+          full_set:
+            type === 'Seat Covers'
+              ? isFullSet(display_set).toLowerCase() == 'full'
+                ? 'Full Seat Set (Front + Rear Seat Set)'
+                : 'Front Seats (Driver + Passenger seats)'
+              : display_set,
+        })
+      );
+      */
+
+  return {
+    main_data,
+    // order_items,
+    shipping_info,
+  };
 };
