0c2f07c62f4aa67d4d94cbddafb5d9128aafabac
coverlandjohnlee <john.l.coverland@gmail.com>
2024-07-02 13:04:34 -0700

    sets delivery confirmation sg email template id

--- src/lib/sendgrid/emails/delivery-confirmation.ts
+++ src/lib/sendgrid/emails/delivery-confirmation.ts
@@ -3,4 +3,4 @@ import sgMail, { MailDataRequired } from '@sendgrid/mail';
 sgMail.setApiKey(process.env.SENDGRID_API_KEY || '');
 const sgFromEmail = process.env.SENDGRID_FROM_EMAIL;
 const sgShippingConfirmationTemplateId =
-  process.env.SENDGRID_SHIPPING_CONFIRMATION_EMAIL_TEMPLATE_ID;
+  process.env.SENDGRID_DELIVERY_CONFIRMATION_EMAIL_TEMPLATE_ID;
