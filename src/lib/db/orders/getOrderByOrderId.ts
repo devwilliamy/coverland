9eb834f9229cf88787ad149e8679aa2b641c998d
coverlandjohnlee <john.l.coverland@gmail.com>
2024-07-02 10:34:30 -0700

    error handling (bubbling up from fetchUserOrderById), internal status 500, etc

--- src/lib/db/orders/getOrderByOrderId.ts
+++ src/lib/db/orders/getOrderByOrderId.ts
@@ -31,27 +31,27 @@ export type TOrderItemProduct = TInitialProductDataDB & {
   discount: number | string; // there is no discount property in the supabase model for Products
 };
 
-
 async function fetchUserOrder(orderId: number): Promise<TUserOrder | null> {
   const supabase = supabaseDatabaseClient;
 
-    try {
-      const { data, error } = await supabase
-        .from<TUserOrder>(ADMIN_PANEL_ORDERS)
-        .select('*')
-        .eq('id', orderId)
-        .single();
+  try {
+    const { data, error } = await supabase
+      .from<TUserOrder>(ADMIN_PANEL_ORDERS)
+      .select('*')
+      .eq('id', orderId)
+      .single();
 
-      if (error) {
-        console.error('Error fetching User Order from supabase:', error);
-        return null;
-      }
-
-      return data || null;
-    } catch (err) {
-      console.error('Unexpected error fetching User Order:', err);
-      return null;
+    if (error) {
+      throw new Error(
+        `Error fetching User Order from supabase: ${error.message}`
+      );
     }
+
+    return data || null;
+  } catch (err) {
+    console.error('Unexpected error fetching User Order:', err);
+    throw err;
+  }
 }
 
 async function fetchOrderItems(
@@ -65,14 +65,15 @@ async function fetchOrderItems(
       .in('order_id', orderIds);
 
     if (error) {
-      console.error('Error fetching order items from supabase:', error);
-      return null;
+      throw new Error(
+        `Error fetching order items from supabase: ${error.message}`
+      );
     }
 
     return data;
   } catch (err) {
     console.error('Unexpected error fetching order items:', err);
-    return null;
+    throw err;
   }
 }
 
@@ -93,81 +94,70 @@ async function fetchOrderItemProducts(
 
     return data;
   } catch (err) {
-    console.error('Unexpected error fetching products:', err);
-    return null;
+    throw new Error(`Error fetching products from supabase: ${error.message}`);
   }
 }
 
 export async function fetchUserOrderById(
-    orderId: number
-  ): Promise<TUserOrder | null> {
-    // fetch user order by id
-    try {
-      const order = await fetchUserOrder(orderId);
-      if (!order) return null;
-  
-      try {
-        const orderItems = await fetchOrderItems([order.id]);
-        if (!orderItems) return null;
-  
-        const productIds = orderItems.map((item) => item.product_id);
-  
-        try {
-          const products = await fetchOrderItemProducts(productIds);
-          if (!products) return null;
-  
-          // Filter items that belong to the order and map them to include product information
-          const items = orderItems
-            .filter((item) => item.order_id === order.id)
-            .map((item) => {
-              const product = products.find(
-                (product) => product.id === item.product_id
-              );
-  
-              if (!product) {
-                // Handle case where product is not found
-                console.error(`Product not found for item id ${item.id}`);
-                return null;
-              }
-  
-              const productWithDiscount: TOrderItemProduct = {
-                ...product,
-                discount: '', // Calculate and add a product discount property
-              };
-  
-              return {
-                ...item,
-                price: formatMoneyAsNumber(item.price) || item.price,
-                product: productWithDiscount,
-              };
-            })
-            .filter((item) => item !== null); // Remove null items if any
-  
-          const orderWithItemsAndProducts = {
-            ...order,
-            total_amount:
-              formatMoneyAsNumber(order.total_amount) || order.total_amount,
-            payment_date: formatISODate(order.payment_date) || order.payment_date,
-            items,
-            total_original_amount:
-              formatMoneyAsNumber(order.total_original_amount) ||
-              order.total_original_amount,
-            total_discount_amount:
-              formatMoneyAsNumber(order.total_discount_amount) ||
-              order.total_discount_amount,
-          };
-  
-          return orderWithItemsAndProducts;
-        } catch (err) {
-          console.error('Unexpected error fetching products:', err);
+  orderId: number
+): Promise<TUserOrder | null> {
+  // fetch user order by id
+  try {
+    const order = await fetchUserOrder(orderId);
+    if (!order) return null;
+
+    const orderItems = await fetchOrderItems([order.id]);
+    if (!orderItems) return null;
+
+    const productIds = orderItems.map((item) => item.product_id);
+
+    const products = await fetchOrderItemProducts(productIds);
+    if (!products) return null;
+
+    // Filter items that belong to the order and map them to include product information
+    const items = orderItems
+      .filter((item) => item.order_id === order.id)
+      .map((item) => {
+        const product = products.find(
+          (product) => product.id === item.product_id
+        );
+
+        if (!product) {
+          // Handle case where product is not found
+          console.error(`Product not found for item id ${item.id}`);
           return null;
         }
-      } catch (err) {
-        console.error('Unexpected error fetching order items:', err);
-        return null;
-      }
-    } catch (err) {
-      console.error('Unexpected error fetching user order:', err);
-      return null;
-    }
+
+        const productWithDiscount: TOrderItemProduct = {
+          ...product,
+          discount: '', // Calculate and add a product discount property
+        };
+
+        return {
+          ...item,
+          price: formatMoneyAsNumber(item.price) || item.price,
+          product: productWithDiscount,
+        };
+      })
+      .filter((item) => item !== null); // Remove null items if any
+
+    const orderWithItemsAndProducts = {
+      ...order,
+      total_amount:
+        formatMoneyAsNumber(order.total_amount) || order.total_amount,
+      payment_date: formatISODate(order.payment_date) || order.payment_date,
+      items,
+      total_original_amount:
+        formatMoneyAsNumber(order.total_original_amount) ||
+        order.total_original_amount,
+      total_discount_amount:
+        formatMoneyAsNumber(order.total_discount_amount) ||
+        order.total_discount_amount,
+    };
+
+    return orderWithItemsAndProducts;
+  } catch (err) {
+    console.error('Unexpected error fetching user order:', err);
+    throw err;
   }
+}
