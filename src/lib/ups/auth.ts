b6088e48b5370200a9c783e2a6f303a192dfdf12
coverlandjohnlee <john.l.coverland@gmail.com>
2024-07-02 17:13:59 -0700

    ups oauth(required) file

---
+++ src/lib/ups/auth.ts
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+const UPS_CLIENT_ID = process.env.UPS_CLIENT_ID;
+const UPS_CLIENT_SECRET = process.env.UPS_CLIENT_SECRET;
+const UPS_USER_ID = process.env.UPS_USER_ID;
+const UPS_PASSWORD = process.env.UPS_PASSWORD;
