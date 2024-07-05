125b432f9b7db222dec4da30bf3048326016a3a8
coverlandjohnlee <john.l.coverland@gmail.com>
2024-07-05 11:39:16 -0700

    refactors typing, and reformats

--- src/lib/db/profile/utils/getPaymentMethodDetails/index.ts
+++ src/lib/db/profile/utils/getPaymentMethodDetails/index.ts
@@ -1,89 +1,114 @@
 import cardLogo from '@/images/profile/orders/card.svg';
 import { capitalizeString } from '@/lib/utils/stringFuncs';
-import { TUserOrder, TInitialOrderItemsDataDB } from '@/lib/db/profile/ordersHistory';
+import {
+  TUserOrder,
+  TInitialOrderItemsDataDB,
+} from '@/lib/db/profile/ordersHistory';
+
+type PaymentMethod = 'link' | 'card' | 'klarna' | 'paypal' | 'default';
+type WalletType = 'google_pay' | 'apple_pay';
+
+type PaymentDetails = {
+  alt: string;
+  text: string;
+  logo: string;
+  size: number;
+};
+
+const paymentMethods: Record<PaymentMethod, PaymentDetails> = {
+  link: {
+    alt: 'Card Link',
+    text: 'Card Link',
+    logo: cardLogo,
+    size: 22,
+  },
+  card: {
+    alt: 'Card',
+    text: 'Card',
+    logo: '',
+    size: 0,
+  },
+  klarna: {
+    alt: 'Klarna Logo',
+    text: 'Klarna',
+    logo: '/images/profile/orders/klarna.svg',
+    size: 35,
+  },
+  paypal: {
+    alt: 'PayPal',
+    text: 'PayPal',
+    logo: '/images/profile/orders/paypal-color-icon.webp',
+    size: 35,
+  },
+  default: {
+    alt: 'Card',
+    text: 'Card',
+    logo: cardLogo,
+    size: 22,
+  },
+};
+
+const walletTypes: Record<WalletType, PaymentDetails> = {
+  google_pay: {
+    alt: 'Google Pay',
+    text: 'Google Pay',
+    logo: '/images/profile/orders/google_pay.svg',
+    size: 40,
+  },
+  apple_pay: {
+    alt: 'Apple Pay',
+    text: 'Apple Pay',
+    logo: '/images/profile/orders/apple_pay.svg',
+    size: 35,
+  },
+};
 
 /**
- * 
+ *
  * @param order TUserOrder | TInitialOrderItemsDataDB (takes a general order from DB or the modified User Order)
  * @returns paymentMethod, which is an object with image logoPath, image alt, image size, and display text
  */
-export const getPaymentMethodDetails = (order: TUserOrder | TInitialOrderItemsDataDB ) => {
-    const paymentMethods = {
-      link: {
-        alt: 'Card Link',
-        text: 'Card Link',
-        logo: '/images/profile/orders/card.svg',
-        size: 22,
-      },
-      card: {
-        alt: `${order.card_brand} Card`,
-        text: capitalizeString(order.card_brand) || 'Card',
-        brandLogos: {
-          visa: { logo: '/images/profile/orders/visa.svg', size: 35 },
-          mastercard: {
-            logo: '/images/profile/orders/mc_symbol.svg',
-            size: 33,
-          },
-          amex: { logo: '/images/profile/orders/amex.svg', size: 28 },
-          discover: {
-            logo: '/images/profile/orders/discover-icon.webp',
-            size: 35,
-          },
-        },
-      },
-      klarna: {
-        alt: 'Klarna Logo',
-        text: capitalizeString(order.payment_method) || 'Klarna',
-        logo: '/images/profile/orders/klarna.svg',
-        size: 35,
-      },
-      paypal: {
-        alt: 'PayPal',
-        text: 'PayPal',
-        logo: '/images/profile/orders/paypal-color-icon.webp',
-        size: 35,
-      },
-      default: {
-        alt: 'Card',
-        text: 'Card',
-        logo: cardLogo,
-        size: 22,
-      },
-    };
+export const getPaymentMethodDetails = (
+  order: TUserOrder | TInitialOrderItemsDataDB
+): PaymentDetails => {
+  let method: PaymentMethod = order.payment_method || 'default';
+  if (order.payment_method === null && order.payment_gateway === 'paypal') {
+    method = 'paypal';
+  }
+  const paymentMethod = paymentMethods[method];
 
-    const walletTypes = {
-      google_pay: {
-        text: 'Google Pay',
-        logo: '/images/profile/orders/google_pay.svg',
-        size: 40,
-      },
-      apple_pay: {
-        text: 'Apple Pay',
-        logo: '/images/profile/orders/apple_pay.svg',
-        size: 35,
-      },
-    };
-
-    const method =
-      order.payment_method ||
-      (order.payment_gateway === 'paypal' ? 'paypal' : 'default');
-    const paymentMethod = paymentMethods[method] || paymentMethods.default;
-
-    if (method === 'card' && order.card_brand) {
-      const brandDetails = paymentMethod.brandLogos[order.card_brand];
-      if (brandDetails) {
-        paymentMethod.logo = brandDetails.logo;
-        paymentMethod.size = brandDetails.size;
-      }
+  if (method === 'card' && order.card_brand) {
+    switch (order.card_brand) {
+      case 'visa':
+        paymentMethod.logo = '/images/profile/orders/visa.svg';
+        paymentMethod.size = 35;
+        break;
+      case 'mastercard':
+        paymentMethod.logo = '/images/profile/orders/mc_symbol.svg';
+        paymentMethod.size = 33;
+        break;
+      case 'amex':
+        paymentMethod.logo = '/images/profile/orders/amex.svg';
+        paymentMethod.size = 28;
+        break;
+      case 'discover':
+        paymentMethod.logo = '/images/profile/orders/discover-icon.webp';
+        paymentMethod.size = 35;
+        break;
     }
+    paymentMethod.alt = `${order.card_brand} Card`;
+    paymentMethod.text = capitalizeString(order.card_brand) || 'Card';
+  }
 
-    // note for apple pay and google pay, the order also has a credit card type but I am overwriting it with the wallet type
-    if (order.wallet_type && walletTypes[order.wallet_type]) {
-      const wallet = walletTypes[order.wallet_type];
-      paymentMethod.logo = wallet.logo;
-      paymentMethod.text = wallet.text;
-      paymentMethod.size = wallet.size;
-    }
+  if (order.wallet_type) {
+    const wallet = walletTypes[order.wallet_type];
+    return {
+      ...paymentMethod,
+      logo: wallet.logo,
+      text: wallet.text,
+      size: wallet.size,
+    };
+  }
 
-    return paymentMethod;
-  };
+  return paymentMethod;
+};
