66e647fdfd01500f939dc08f1c850c2f8715fcb3
coverlandjohnlee <john.l.coverland@gmail.com>
2024-07-11 16:56:39 -0700

    switches back databaseTableNames

--- src/lib/db/constants/databaseTableNames.ts
+++ src/lib/db/constants/databaseTableNames.ts
@@ -2,14 +2,8 @@
 export const PRODUCT_REVIEWS_TABLE = 'reviews-2';
 export const CAR_COVERS_REVIEWS_TABLE = 'reviews_car_covers';
 export const SEAT_COVERS_REVIEWS_TABLE = 'reviews_seat_covers';
-export const PRODUCT_DATA_TABLE = 
-process.env.NEXT_PUBLIC_IS_PREVIEW === 'PREVIEW'
-? 'Products_preorder_withNewSeatCovers_20240709'
-: 'Products';
-export const RELATIONS_PRODUCT_TABLE =
-process.env.NEXT_PUBLIC_IS_PREVIEW === 'PREVIEW'
-? 'relations_product_20240709'
-: 'relations_product';
+export const PRODUCT_DATA_TABLE = 'Products';
+export const RELATIONS_PRODUCT_TABLE = 'relations_product';
 export const TYPE_TABLE = 'Type';
 export const TYPE_MAKE_YEAR_DISTINCT = 'type_make_year_distinct';
 export const ADMIN_PANEL_ORDERS =
@@ -30,9 +24,5 @@ export const RPC_GET_MAKE_RELATION = 'get_make_relation';
 export const RPC_GET_UNIQUE_YEARS = 'get_unique_years';
 export const RPC_GET_DISTINCT_REVIEW_IMAGES = 'get_distinct_review_images';
 export const RPC_GET_PRODUCT_REVIEWS_SUMMARY = 'get_product_reviews_summary';
-export const RPC_GET_SEAT_COVER_SORTED_BY_COLOR =
-process.env.NEXT_PUBLIC_IS_PREVIEW === 'PREVIEW'
-? 'get_seat_cover_products_sorted_by_color_preorder_20240709'
-: 'get_seat_cover_products_sorted_by_color';
-
+export const RPC_GET_SEAT_COVER_SORTED_BY_COLOR = 'get_seat_cover_products_sorted_by_color';
   
