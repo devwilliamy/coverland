1fe376aa4d6b6892762bc61785232c559b90c84b
William Yang <dev.william.coverland@gmail.com>
2024-07-12 09:15:53 -0700

    Updated banner with automatically changing months

--- src/lib/utils/date.ts
+++ src/lib/utils/date.ts
@@ -70,3 +70,12 @@ export function weeksFromCurrentDate(targetDate: string): number {
 
   return Math.ceil(diffInWeeks);
 }
+
+export function getCurrentMonth() {
+  const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+  const date = new Date();
+  return monthNames[date.getMonth()];
+};
