c23c1719cabf6668132de1205076f5d22eb98298
William Yang <dev.william.y@icarcover.com>
2024-08-26 15:39:35 -0700

    Made preorder into two steps. Added checkbox for users to confirm preorder. Checkout is disabled if check box isn't checked

---
+++ src/components/cart/PreorderConfirm.tsx
@@ -0,0 +1,60 @@
+import { formatISODateNoTimeZone } from '@/lib/utils/date';
+import { Checkbox } from '../ui/checkbox';
+import LineSeparator from '../ui/line-separator';
+import useStoreContext from '@/hooks/useStoreContext';
+import { useStore } from 'zustand';
+
+type PreorderConfirmProps = {
+  checked?: boolean;
+  disabled?: boolean;
+  onChange?: (checked: boolean) => void;
+  className?: string;
+  setChecked: any;
+};
+
+export default function PreorderConfirm({
+  checked = false,
+  disabled = false,
+  setChecked,
+}: PreorderConfirmProps) {
+  const store = useStoreContext();
+  if (!store) throw new Error('Missing Provider in the tree');
+  const selectedProduct = useStore(store, (s) => s.selectedProduct);
+
+  return (
+    <>
+      <div className="px-2 py-4">
+        <div className="pb-4 text-center">
+          <p className="py-4 text-2xl font-extrabold">
+            This is a Pre-Order Item!
+          </p>
+          <div className="mb-3 mt-1 flex items-center justify-center space-x-2">
+            <div className="mx-2 text-xl font-bold text-[#2BA45B]">
+              Est. Ship Date:{' '}
+              {formatISODateNoTimeZone(selectedProduct?.preorder_date ?? '')}
+            </div>
+          </div>
+        </div>
+        <LineSeparator className="mb-2" />
+        <div className="flex py-8">
+          <Checkbox
+            id="preorder-acceptance"
+            checked={checked}
+            disabled={disabled}
+            onCheckedChange={setChecked}
+            className="mt-1 rounded-md border-[#343434]"
+          />
+          <label
+            htmlFor="preorder-acceptance"
+            className={`ml-2 text-sm font-normal leading-6 peer-disabled:cursor-not-allowed peer-disabled:opacity-70 lg:text-base lg:font-medium ${
+              disabled ? 'cursor-not-allowed text-gray-500' : 'cursor-pointer'
+            }`}
+          >
+            I acknowledge that this is a preorder and understand my delivery will
+            be delayed.
+          </label>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/cart/PreorderSheet.tsx
+++ src/components/cart/PreorderSheet.tsx
@@ -8,13 +8,15 @@ import {
 } from '@/components/ui/sheet';
 import { IoClose } from 'react-icons/io5';
 import PreorderHeader from '../cart/PreorderHeader';
-import PreorderBody from '../cart/PreorderBody';
+import PreorderPreview from '../cart/PreorderPreview';
 import PreorderFooter from '../cart/PreorderFooter';
 import { TInitialProductDataDB } from '@/lib/db';
 import { IProductData } from '@/utils';
 import { useMediaQuery } from '@mantine/hooks';
 import { Separator } from '../ui/separator';
 import { TSeatCoverDataDB } from '@/lib/db/seat-covers';
+import { useState } from 'react';
+import PreorderConfirm from './PreorderConfirm';
 
 type PreorderSheetProps = {
   open: boolean;
@@ -26,12 +28,44 @@ type PreorderSheetProps = {
     | undefined
     | TSeatCoverDataDB;
 };
+
+export type PreorderStep = 'preview' | 'confirm';
+
+export const PREORDER_PREVIEW = 'preview';
+export const PREORDER_CONFIRM = 'confirm';
 const PreorderSheet = ({
   open,
   setOpen,
   selectedProduct,
 }: PreorderSheetProps): JSX.Element => {
   const isMobile = useMediaQuery('(max-width: 1023px)');
+  const [currentStep, setCurrentStep] =
+    useState<PreorderStep>(PREORDER_PREVIEW);
+  const [checked, setChecked] = useState<boolean>(false);
+  const handleNextStep = () => {
+    switch (currentStep) {
+      case PREORDER_PREVIEW:
+        setCurrentStep(PREORDER_CONFIRM);
+        break;
+    }
+  };
+
+  const handlePreviousStep = () => {
+    switch (currentStep) {
+      case PREORDER_CONFIRM:
+        setCurrentStep(PREORDER_PREVIEW);
+        break;
+    }
+  };
+
+  const renderStepContent = () => {
+    switch (currentStep) {
+      case PREORDER_PREVIEW:
+        return <PreorderPreview selectedProduct={selectedProduct} />;
+      case PREORDER_CONFIRM:
+        return <PreorderConfirm checked={checked} setChecked={setChecked} />;
+    }
+  };
   return (
     <Sheet open={open} onOpenChange={setOpen}>
       <SheetContent
@@ -40,14 +74,20 @@ const PreorderSheet = ({
       >
         <SheetHeader>
           <SheetTitle className="flex w-full items-center justify-between py-7 pl-4 pr-7">
-            <PreorderHeader />
+            <PreorderHeader
+              currentStep={currentStep}
+              handlePreviousStep={handlePreviousStep}
+            />
             <SheetClose
               asChild
               className="cursor-pointer bg-gray-200 text-black *:h-6 *:w-6"
             >
               <button
                 className="rounded-full p-[5px]"
-                onClick={() => setOpen(false)}
+                onClick={() => {
+                  setOpen(false);
+                  setCurrentStep('preview');
+                }}
               >
                 <IoClose />
               </button>
@@ -55,13 +95,15 @@ const PreorderSheet = ({
           </SheetTitle>
           <Separator />
         </SheetHeader>
-        <div className=" flex w-full flex-col px-4">
-          <PreorderBody selectedProduct={selectedProduct} />
-        </div>
+        <div className=" flex w-full flex-col px-4">{renderStepContent()}</div>
         <div className="w-full bg-white shadow-[0_-4px_4px_-0px_rgba(0,0,0,0.1)]">
           <SheetFooter>
             <SheetClose asChild>
-              <PreorderFooter />
+              <PreorderFooter
+                handleNextStep={handleNextStep}
+                currentStep={currentStep}
+                checked={checked}
+              />
             </SheetClose>
           </SheetFooter>
         </div>
