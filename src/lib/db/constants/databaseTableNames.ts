10718125f99d3d6b5fae8f5c5a52382f4cdc809a
William Yang <dev.william.y@icarcover.com>
2024-08-28 17:30:39 -0700

    Added icons for new seat cover colors. Added new default stuff for seat covers page

--- src/lib/db/constants/databaseTableNames.ts
+++ src/lib/db/constants/databaseTableNames.ts
@@ -2,8 +2,8 @@
 export const PRODUCT_REVIEWS_TABLE = 'reviews-2';
 export const CAR_COVERS_REVIEWS_TABLE = 'reviews_car_covers';
 export const SEAT_COVERS_REVIEWS_TABLE = 'reviews_seat_covers';
-export const PRODUCT_DATA_TABLE = 'Products';
-export const RELATIONS_PRODUCT_TABLE = 'relations_product';
+export const PRODUCT_DATA_TABLE = 'Products_20240828';
+export const RELATIONS_PRODUCT_TABLE = 'relations_product_20240828';
 export const TYPE_TABLE = 'Type';
 export const TYPE_MAKE_YEAR_DISTINCT = 'type_make_year_distinct';
 export const ADMIN_PANEL_ORDERS =
@@ -29,6 +29,6 @@ export const RPC_GET_UNIQUE_YEARS = 'get_unique_years';
 export const RPC_GET_DISTINCT_REVIEW_IMAGES = 'get_distinct_review_images';
 export const RPC_GET_PRODUCT_REVIEWS_SUMMARY = 'get_product_reviews_summary';
 export const RPC_GET_SEAT_COVER_SORTED_BY_COLOR =
-  'get_seat_cover_products_sorted_by_color';
+  'get_seat_cover_products_sorted_by_color_20240828';
 export const RPC_GET_DEFAULT_SEAT_COVER_SORTED_BY_COLOR =
-  'get_default_seat_cover_products_sorted_by_color';
+  'get_default_seat_cover_products_sorted_by_color_20240828';
