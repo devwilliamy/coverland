18558d126e6a29729b36af77b2ced033d22df30e
William Yang <dev.william.y@icarcover.com>
2024-09-27 11:14:08 -0700

    Fixed product title a little

--- src/app/(main)/[productType]/components/ProductTitle.tsx
+++ src/app/(main)/[productType]/components/ProductTitle.tsx
@@ -13,20 +13,18 @@ const ProductTitle = () => {
   return (
     <h1 className="mt-[24px] text-[24px] font-[900] leading-[27px] text-[#1A1A1A] lg:mt-0 lg:text-[28px] lg:leading-[30px] ">
       {/* TODO: Remember to check Rivian when it's available */}
-      {/* {!make && !model ? (
+      {!make && !model ? (
         <>Waterproof Outdoor Custom-Fit {`${productType} `}</>
       ) : (
         <>
-          {make === 'rivian' && 'Car Cover for '}
+          {/* {make === 'rivian' && 'Car Cover for '}
           {make && (selectedProduct.make as string)}{' '}
           {model && (selectedProduct.model as string)}{' '}
           {make === 'rivian' ? '' : `${productType} `}
-          {' - '}
-          <br />
-          <>Waterproof, Outdoor, Custom-Fit</>
+          {' - '} */}
+          {selectedProduct.title}
         </>
-      )} */}
-      {selectedProduct.title}
+      )}
     </h1>
   );
 };
